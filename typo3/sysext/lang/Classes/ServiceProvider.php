bbea53dc976a417dec4bde5ab865b912b01d63a0
Benjamin Franzke <bfr@qbus.de>
2018-02-13 14:37:08 +0100

    [TASK] Generate service providers for current set of singleton classes
    Service providers have been generated with:
    
    find typo3/sysext/ -maxdepth 3 -name 'ServiceProvider.php' | xargs rm
    ls -1 typo3/sysext/ | while read ext; do
        Build/Scripts/createServiceProvider.sh ${ext}
    done
    
    Change-Id: Idfdeda3140c591bd03fc63aded67cd3aff9e2adf
    Releases: master
    Resolves: #?

---
+++ typo3/sysext/lang/Classes/ServiceProvider.php
@@ -0,0 +1,41 @@
+<?php
+declare(strict_types = 1);
+namespace TYPO3\CMS\Lang;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use Psr\Container\ContainerInterface;
+use TYPO3\CMS\Core\Core\AbstractServiceProvider;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+class ServiceProvider extends AbstractServiceProvider
+{
+    public function getFactories(): array
+    {
+        return [
+            Service\TerService::class => [ static::class, 'getTerService' ],
+            Service\TranslationService::class => [ static::class, 'getTranslationService' ],
+        ];
+    }
+
+    public static function getTerService(ContainerInterface $container): Service\TerService
+    {
+        return GeneralUtility::makeInstance(Service\TerService::class);
+    }
+
+    public static function getTranslationService(ContainerInterface $container): Service\TranslationService
+    {
+        return GeneralUtility::makeInstance(Service\TranslationService::class);
+    }
+}
