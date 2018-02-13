7e43d8a3796f457fcfe7489f75d71868b6647093
Benjamin Franzke <bfr@qbus.de>
2018-02-13 14:37:08 +0100

    [TASK] Generate service providers for current set of singleton classes
    Service providers have been generated with:
    
    find typo3/sysext/ -maxdepth 3 -name 'ServiceProvider.php' | xargs rm
    ls -1 typo3/sysext/ | while read ext; do
        Build/Scripts/createServiceProvider.sh ${ext}
    done
    
    Change-Id: Idfdeda3140c591bd03fc63aded67cd3aff9e2adf
    Releases: master
    Resolves: #?

---
+++ typo3/sysext/extensionmanager/Classes/ServiceProvider.php
@@ -0,0 +1,83 @@
+<?php
+declare(strict_types = 1);
+namespace TYPO3\CMS\Extensionmanager;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use Psr\Container\ContainerInterface;
+use TYPO3\CMS\Core\Core\AbstractServiceProvider;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+class ServiceProvider extends AbstractServiceProvider
+{
+    public function getFactories(): array
+    {
+        return [
+            Domain\Model\DownloadQueue::class => [ static::class, 'getDownloadQueue' ],
+            Service\ExtensionManagementService::class => [ static::class, 'getExtensionManagementService' ],
+            Utility\DependencyUtility::class => [ static::class, 'getDependencyUtility' ],
+            Utility\DownloadUtility::class => [ static::class, 'getDownloadUtility' ],
+            Utility\EmConfUtility::class => [ static::class, 'getEmConfUtility' ],
+            Utility\FileHandlingUtility::class => [ static::class, 'getFileHandlingUtility' ],
+            Utility\InstallUtility::class => [ static::class, 'getInstallUtility' ],
+            Utility\ListUtility::class => [ static::class, 'getListUtility' ],
+            Utility\Repository\Helper::class => [ static::class, 'getHelper' ],
+        ];
+    }
+
+    public static function getDownloadQueue(ContainerInterface $container): Domain\Model\DownloadQueue
+    {
+        return GeneralUtility::makeInstance(Domain\Model\DownloadQueue::class);
+    }
+
+    public static function getExtensionManagementService(ContainerInterface $container): Service\ExtensionManagementService
+    {
+        return GeneralUtility::makeInstance(Service\ExtensionManagementService::class);
+    }
+
+    public static function getDependencyUtility(ContainerInterface $container): Utility\DependencyUtility
+    {
+        return GeneralUtility::makeInstance(Utility\DependencyUtility::class);
+    }
+
+    public static function getDownloadUtility(ContainerInterface $container): Utility\DownloadUtility
+    {
+        return GeneralUtility::makeInstance(Utility\DownloadUtility::class);
+    }
+
+    public static function getEmConfUtility(ContainerInterface $container): Utility\EmConfUtility
+    {
+        return GeneralUtility::makeInstance(Utility\EmConfUtility::class);
+    }
+
+    public static function getFileHandlingUtility(ContainerInterface $container): Utility\FileHandlingUtility
+    {
+        return GeneralUtility::makeInstance(Utility\FileHandlingUtility::class);
+    }
+
+    public static function getInstallUtility(ContainerInterface $container): Utility\InstallUtility
+    {
+        return GeneralUtility::makeInstance(Utility\InstallUtility::class);
+    }
+
+    public static function getListUtility(ContainerInterface $container): Utility\ListUtility
+    {
+        return GeneralUtility::makeInstance(Utility\ListUtility::class);
+    }
+
+    public static function getHelper(ContainerInterface $container): Utility\Repository\Helper
+    {
+        return GeneralUtility::makeInstance(Utility\Repository\Helper::class);
+    }
+}
---
+++ typo3/sysext/rsaauth/Classes/ServiceProvider.php
@@ -0,0 +1,47 @@
+<?php
+declare(strict_types = 1);
+namespace TYPO3\CMS\Rsaauth;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use Psr\Container\ContainerInterface;
+use TYPO3\CMS\Core\Core\AbstractServiceProvider;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+class ServiceProvider extends AbstractServiceProvider
+{
+    public function getFactories(): array
+    {
+        return [
+            Keypair::class => [ static::class, 'getKeypair' ],
+            RsaEncryptionDecoder::class => [ static::class, 'getRsaEncryptionDecoder' ],
+            RsaEncryptionEncoder::class => [ static::class, 'getRsaEncryptionEncoder' ],
+        ];
+    }
+
+    public static function getKeypair(ContainerInterface $container): Keypair
+    {
+        return GeneralUtility::makeInstance(Keypair::class);
+    }
+
+    public static function getRsaEncryptionDecoder(ContainerInterface $container): RsaEncryptionDecoder
+    {
+        return GeneralUtility::makeInstance(RsaEncryptionDecoder::class);
+    }
+
+    public static function getRsaEncryptionEncoder(ContainerInterface $container): RsaEncryptionEncoder
+    {
+        return GeneralUtility::makeInstance(RsaEncryptionEncoder::class);
+    }
+}
