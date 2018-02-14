3e82074553923cd2b5a5d5ce23efbe69e168396e
Benjamin Franzke <bfr@qbus.de>
2018-02-14 19:24:24 +0100

    [WIP][FEATURE] Introduce PSR-11 container (simplex) and service providers
    The container is implemented using Simplex.
    Simplex is a Pimple fork that adds support
    for container-interop/service-provider.
    
    Service providers (and so does pimple) provide
    factories through php code. No configuration.
    
    composer require container-interop/service-provider:~0.4.0
    composer require mnapoli/simplex:~0.5.0
    
    Change-Id: Ib13b2ff74ed2341f6f5788c641128a519cad5d15
    Releases: master
    Resolves: #?

---
+++ typo3/sysext/core/Classes/Core/AbstractServiceProvider.php
@@ -0,0 +1,57 @@
+<?php
+declare(strict_types = 1);
+namespace TYPO3\CMS\Core\Core;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use Interop\Container\ServiceProviderInterface;
+use Psr\Container\ContainerInterface;
+use Psr\Log\LoggerAwareInterface;
+use TYPO3\CMS\Core\Log\LogManager;
+use TYPO3\CMS\Core\SingletonInterface;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+abstract class AbstractServiceProvider implements ServiceProviderInterface
+{
+    public function getFactories(): array
+    {
+        return [];
+    }
+
+    public function getExtensions(): array
+    {
+        return [];
+    }
+
+    /**
+     * Create an instance of a class
+     *
+     * Supports auto injection of the logger and registering as (legacy) singleton instance.
+     *
+     * @param ContainerInterface $container
+     * @param string $className name of the class to instantiate, must not be empty and not start with a backslash
+     * @param array $constructorArguments Arguments for the constructor
+     */
+    protected static function new(ContainerInterface $container, string $className, array $constructorArguments = [])
+    {
+        $instance = new $className(...$constructorArguments);
+        if ($instance instanceof SingletonInterface) {
+            GeneralUtility::setSingletonInstance($className, $instance);
+        }
+        if ($instance instanceof LoggerAwareInterface) {
+            $instance->setLogger($container->get(LogManager::class)->getLogger($className));
+        }
+        return $instance;
+    }
+}
