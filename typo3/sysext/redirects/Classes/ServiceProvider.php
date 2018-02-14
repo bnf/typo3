f8d454d0e46d8de9951bdccb67b6b58e3d1656b7
Benjamin Franzke <bfr@qbus.de>
2018-02-14 14:17:45 +0100

    [TASK] Example: Initialize redirect handler through the container
    This demonstrates what a service provider
    (for a rather simple class) would look like.
    
    Change-Id: Id1a839e8b00432f73e46e29daa9756acefe8e838

---
+++ typo3/sysext/redirects/Classes/ServiceProvider.php
@@ -0,0 +1,49 @@
+<?php
+declare(strict_types = 1);
+namespace TYPO3\CMS\Redirects;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use Psr\Container\ContainerInterface;
+use TYPO3\CMS\Core\Core\AbstractServiceProvider;
+use TYPO3\CMS\Core\Log\LogManager;
+
+class ServiceProvider extends AbstractServiceProvider
+{
+    public function getFactories(): array
+    {
+        return [
+            Http\Middleware\RedirectHandler::class => [ static::class, 'getRedirectHandler' ],
+            Service\RedirectService::class => [ static::class, 'getRedirectService' ],
+        ];
+    }
+
+    public static function getRedirectHandler(ContainerInterface $container): Http\Middleware\RedirectHandler
+    {
+        return static::new(
+            $container,
+            Http\Middleware\RedirectHandler::class,
+            [
+                $container->get(Service\RedirectService::class),
+                new \TYPO3\CMS\Core\Configuration\Features,
+                new \TYPO3\CMS\Core\Database\ConnectionPool
+            ]
+        );
+    }
+
+    public static function getRedirectService(ContainerInterface $container): Service\RedirectService
+    {
+        return static::new($container, Service\RedirectService::class);
+    }
+}
