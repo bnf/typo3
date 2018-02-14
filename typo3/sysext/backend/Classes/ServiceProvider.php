c2c614f72d724756f504c0e3b62d484742922518
Benjamin Franzke <bfr@qbus.de>
2018-02-14 13:20:36 +0100

    [FEAUTURE] Support instantiating middlewares through the PSR-11 container
    Change-Id: I876aa5095fd0772990c1e5f8e99c6c1163f0c779

--- typo3/sysext/backend/Classes/ServiceProvider.php
+++ typo3/sysext/backend/Classes/ServiceProvider.php
@@ -75,7 +75,8 @@ class ServiceProvider extends AbstractServiceProvider
 
         $requestHandler = new \TYPO3\CMS\Core\Http\MiddlewareDispatcher(
             $container->get(Http\RequestHandler::class),
-            $container->get('backend.middlewares')
+            $container->get('backend.middlewares'),
+            $container
         );
 
         return new Http\Application($requestHandler);
