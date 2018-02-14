76bd86602aae2a705483069f35c0ffa7da0fd9ad
Benjamin Franzke <bfr@qbus.de>
2018-02-14 18:45:09 +0100

    [TASK] Initialize TCA through PSR-11 container and service provider
    Note: This drops TCA caching for now.
    Change-Id: I92feb75bd113f71aac889ece9b750bbbfedd1026

--- typo3/sysext/core/Classes/Core/AbstractServiceProvider.php
+++ typo3/sysext/core/Classes/Core/AbstractServiceProvider.php
@@ -16,6 +16,7 @@ namespace TYPO3\CMS\Core\Core;
  */
 
 use Interop\Container\ServiceProviderInterface;
+use Psr\Container\ContainerInterface;
 
 abstract class AbstractServiceProvider implements ServiceProviderInterface
 {
@@ -36,6 +37,58 @@ abstract class AbstractServiceProvider implements ServiceProviderInterface
 
     public function getExtensions(): array
     {
-        return [];
+        return [
+            'TCAConfiguration' => function (ContainerInterface $container, array $TCA): array {
+                $tcaConfigurationDirectory = $this->path . 'Configuration/TCA';
+                if (is_dir($tcaConfigurationDirectory)) {
+                    $files = scandir($tcaConfigurationDirectory);
+                    foreach ($files as $file) {
+                        if (
+                            is_file($tcaConfigurationDirectory . '/' . $file)
+                            && ($file !== '.')
+                            && ($file !== '..')
+                            && (substr($file, -4, 4) === '.php')
+                        ) {
+                            $tcaOfTable = require($tcaConfigurationDirectory . '/' . $file);
+                            if (is_array($tcaOfTable)) {
+                                // TCA table name is filename without .php suffix, eg 'sys_notes', not 'sys_notes.php'
+                                $tcaTableName = substr($file, 0, -4);
+                                $TCA[$tcaTableName] = $tcaOfTable;
+                            }
+                        }
+                    }
+                }
+
+                return $TCA;
+            },
+            'TCAOverrides' => function (ContainerInterface $container, array $TCA): array {
+
+                // Execute override files from Configuration/TCA/Overrides
+                $tcaOverridesPathForPackage = $this->path . 'Configuration/TCA/Overrides';
+                if (!is_dir($tcaOverridesPathForPackage)) {
+                    return $TCA;
+                }
+
+                $files = scandir($tcaOverridesPathForPackage);
+                if (empty($files)) {
+                    return $TCA;
+                }
+
+                $GLOBALS['TCA'] = $TCA;
+                foreach ($files as $file) {
+                    if (
+                        is_file($tcaOverridesPathForPackage . '/' . $file)
+                        && ($file !== '.')
+                        && ($file !== '..')
+                        && (substr($file, -4, 4) === '.php')
+                    ) {
+                        require($tcaOverridesPathForPackage . '/' . $file);
+                    }
+                }
+                $TCA = $GLOBALS['TCA'];
+
+                return $TCA;
+            }
+        ];
     }
 }
--- typo3/sysext/core/Classes/Core/Bootstrap.php
+++ typo3/sysext/core/Classes/Core/Bootstrap.php
@@ -291,7 +291,8 @@ class Bootstrap
             ->loadTypo3LoadedExtAndExtLocalconf(true)
             ->setFinalCachingFrameworkCacheConfiguration()
             ->unsetReservedGlobalVariables()
-            ->loadBaseTca();
+            //->loadBaseTca()
+            ;
         if (empty($GLOBALS['TYPO3_CONF_VARS']['SYS']['encryptionKey'])) {
             throw new \RuntimeException(
                 'TYPO3 Encryption is empty. $GLOBALS[\'TYPO3_CONF_VARS\'][\'SYS\'][\'encryptionKey\'] needs to be set for TYPO3 to work securely',
@@ -975,9 +976,11 @@ class Bootstrap
      * @param bool $allowCaching True, if loading TCA from cache is allowed
      * @return Bootstrap
      * @internal This is not a public API method, do not use in own extensions
+     * @deprecated
      */
     public function loadBaseTca(bool $allowCaching = true): Bootstrap
     {
+        // todo trigger E_USER_DEPRECATED
         ExtensionManagementUtility::loadBaseTca($allowCaching);
         return $this;
     }
@@ -1209,6 +1212,8 @@ class Bootstrap
                 }
             }
         }
+
+        $serviceProviders[] = new PostProcessingServiceProvider;
 
         return $serviceProviders;
     }
