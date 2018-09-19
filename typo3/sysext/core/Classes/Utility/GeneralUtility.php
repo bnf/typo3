163322c464ec92654c7c05e50563ec3dbef8776a
Benjamin Franzke <bfr@qbus.de>
2018-09-19 06:56:40 +0200

    [TMP] ObjectManager->get and makeInstance caller logging
    Change-Id: I0dd8b33a5882af910bc166830c5e11eba9f18581

--- typo3/sysext/core/Classes/Utility/GeneralUtility.php
+++ typo3/sysext/core/Classes/Utility/GeneralUtility.php
@@ -3432,6 +3432,8 @@ class GeneralUtility
         }
         // Return singleton instance if it is already registered
         if (isset(self::$singletonInstances[$finalClassName])) {
+            list($childClass, $caller) = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 2);
+            file_put_contents('./GeneralUtility::makeInstance_get.log', $className . PHP_EOL . '    ' . $caller['class'] . '::' . $caller['function'] . PHP_EOL . '        ' . $childClass['file'] . ' (' . $childClass['line'] . ')' . PHP_EOL, FILE_APPEND);
             return self::$singletonInstances[$finalClassName];
         }
         // Return instance if it has been injected by addInstance()
@@ -3462,6 +3464,9 @@ class GeneralUtility
                 $backtrace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 2);
                 getenv('LOG_FUTURE_DEPRECATIONS') && trigger_error('Singleton ' . $finalClassName . ' is not available in the DI container. That will be required in TYPO3 v11.0. ' . json_encode($backtrace), E_USER_DEPRECATED);
             }
+
+            list($childClass, $caller) = debug_backtrace(false, 2);
+            file_put_contents('./GeneralUtility::makeInstance_get.log', $className . PHP_EOL . '    ' . $caller['class'] . '::' . $caller['function'] . PHP_EOL . '        ' . $childClass['file'] . ' (' . $childClass['line'] . ')' . PHP_EOL, FILE_APPEND);
         }
         if ($instance instanceof LoggerAwareInterface) {
             $instance->setLogger(static::makeInstance(LogManager::class)->getLogger($className));
