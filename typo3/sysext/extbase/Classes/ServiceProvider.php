2c620a9ecc8bdf25d2b5787b412cba00c700f1e6
Benjamin Franzke <bfr@qbus.de>
2018-10-07 16:58:34 +0200

    [WIP][FEATURE] Configure signal slots using DI service tags
    Also configure some slots using service providers, as they
    are needed for the install tool (or are conditionally applied
    depending on the environment).
    
    TODO: deprecate usage in ext_localconf.php?
    
    Change-Id: I441b051932d0450e8f58386d9d4af5ac6df02195

--- typo3/sysext/extbase/Classes/ServiceProvider.php
+++ typo3/sysext/extbase/Classes/ServiceProvider.php
@@ -30,6 +30,7 @@ class ServiceProvider extends AbstractServiceProvider
         return [
             Object\Container\Container::class => [ static::class, 'getObjectContainer' ],
             Object\ObjectManager::class => [ static::class, 'getObjectManager' ],
+            SignalSlot\Dispatcher::class => [ static::class, 'getSignalSlotDispatcher' ],
         ];
     }
 
@@ -43,5 +44,11 @@ class ServiceProvider extends AbstractServiceProvider
         $objectManager = self::new($container, Object\ObjectManager::class, [$container->get(Object\Container\Container::class)]);
         $objectManager->setContainer($container);
         return $objectManager;
+    }
+
+    public static function getSignalSlotDispatcher(ContainerInterface $container): SignalSlot\Dispatcher
+    {
+        $logger = $container->get(\TYPO3\CMS\Core\Log\LogManager::class)->getLogger(SignalSlot\Dispatcher::class);
+        return self::new($container, SignalSlot\Dispatcher::class, [$container->get(Object\ObjectManager::class), $logger]);
     }
 }
