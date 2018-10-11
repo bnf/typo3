dacdd43b500ca02cf17b877a4f297794d4cdb6c9
Benjamin Franzke <bfr@qbus.de>
2018-10-11 09:19:42 +0200

    [TASK] Auto register FAL extractor interfaces using symfony DI
    That happens when the class that implements the particular
    interfaces is set to be autoconfigured.
    
    Change-Id: I6dfd516bf38ed2281403163f63ac17bc80710c2f

--- typo3/sysext/core/Configuration/Services.php
+++ typo3/sysext/core/Configuration/Services.php
@@ -13,6 +13,12 @@ use TYPO3\CMS\Core\DependencyInjection\ControllerWithPsr7ActionMethodsPass;
 use TYPO3\CMS\Core\DependencyInjection\LoggerAwarePass;
 use TYPO3\CMS\Core\DependencyInjection\ResolveGlobalVarsParameterPass;
 use TYPO3\CMS\Core\DependencyInjection\SingletonPass;
+use TYPO3\CMS\Core\Resource\Index\ExtractorInterface;
+use TYPO3\CMS\Core\Resource\Index\ExtractorRegistry;
+use TYPO3\CMS\Core\Resource\Rendering\FileRendererInterface;
+use TYPO3\CMS\Core\Resource\Rendering\RendererRegistry;
+use TYPO3\CMS\Core\Resource\TextExtraction\TextExtractorInterface;
+use TYPO3\CMS\Core\Resource\TextExtraction\TextExtractorRegistry;
 
 return function (ContainerConfigurator $configurator, ContainerBuilder $containerBuilder) {
     $containerBuilder->registerForAutoconfiguration(SingletonInterface::class)->addTag('typo3.singleton');
@@ -21,6 +27,11 @@ return function (ContainerConfigurator $configurator, ContainerBuilder $containe
     // Services, to be read from container-aware dispatchers (on demand), therefore marked 'public'
     $containerBuilder->registerForAutoconfiguration(MiddlewareInterface::class)->addTag('public');
     $containerBuilder->registerForAutoconfiguration(RequestHandlerInterface::class)->addTag('public');
+
+    // FAL registries
+    $containerBuilder->registerForAutoconfiguration(FileRendererInterface::class)->addTag('fal.file_renderer');
+    $containerBuilder->registerForAutoconfiguration(ExtractorInterface::class)->addTag('fal.extractor');
+    $containerBuilder->registerForAutoconfiguration(TextExtractorInterface::class)->addTag('fal.text_extractor');
 
     $containerBuilder->addCompilerPass(new SingletonPass('typo3.singleton'));
     $containerBuilder->addCompilerPass(new LoggerAwarePass('psr.logger_aware'));
@@ -36,6 +47,23 @@ return function (ContainerConfigurator $configurator, ContainerBuilder $containe
             }
             foreach ($container->findTaggedServiceIds('prototype') as $id => $tags) {
                 $container->findDefinition($id)->setShared(false);
+            }
+
+            // FAL registries
+            $rendererRegistry = $container->findDefinition(RendererRegistry::class);
+            foreach ($container->findTaggedServiceIds('fal.file_renderer') as $id => $tags) {
+                $container->findDefinition($id)->setPublic(true);
+                $rendererRegistry->addMethodCall('registerRendererClass', [$id]);
+            }
+            $extractorRegistry = $container->findDefinition(ExtractorRegistry::class);
+            foreach ($container->findTaggedServiceIds('fal.extractor') as $id => $tags) {
+                $container->findDefinition($id)->setPublic(true);
+                $extractorRegistry->addMethodCall('registerExtractionService', [$id]);
+            }
+            $textExtractorRegistry = $container->findDefinition(TextExtractorRegistry::class);
+            foreach ($container->findTaggedServiceIds('fal.text_extractor') as $id => $tags) {
+                $container->findDefinition($id)->setPublic(true);
+                $textExtractorRegistry->addMethodCall('registerTextExtractor', [$id]);
             }
         }
     });
