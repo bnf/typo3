35c23dc7ccc2574f71c2bd505b12c66f1b0fab0d
Benjamin Franzke <bfr@qbus.de>
2018-10-11 09:19:42 +0200

    [TASK] Auto register FAL extractor interfaces using symfony DI
    That happens when the class that implements the particular
    interfaces is set to be autoconfigured.
    
    Change-Id: I6dfd516bf38ed2281403163f63ac17bc80710c2f

--- typo3/sysext/core/Configuration/Services.php
+++ typo3/sysext/core/Configuration/Services.php
@@ -6,7 +6,10 @@ use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 use Psr\Log\LoggerAwareInterface;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+<<<<<<< HEAD
 use Symfony\Component\DependencyInjection\Compiler\PassConfig;
+=======
+>>>>>>> 21c47d93cf... [TASK] Auto register FAL extractor interfaces using symfony DI
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
@@ -18,12 +21,39 @@ return function (ContainerConfigurator $configurator, ContainerBuilder $containe
     $containerBuilder->registerForAutoconfiguration(MiddlewareInterface::class)->addTag('typo3.middleware');
     $containerBuilder->registerForAutoconfiguration(RequestHandlerInterface::class)->addTag('typo3.request_handler');
 
+    // FAL registries
+    $containerBuilder->registerForAutoconfiguration(Resource\Rendering\FileRendererInterface::class)->addTag('fal.file_renderer');
+    $containerBuilder->registerForAutoconfiguration(Resource\Index\ExtractorInterfaceExtractorInterface::class)->addTag('fal.extractor');
+    $containerBuilder->registerForAutoconfiguration(Resource\TextExtraction\TextExtractorInterface::class)->addTag('fal.text_extractor');
+
     $containerBuilder->addCompilerPass(new DependencyInjection\SingletonPass('typo3.singleton'));
     $containerBuilder->addCompilerPass(new DependencyInjection\LoggerAwarePass('psr.logger_aware'));
     $containerBuilder->addCompilerPass(new DependencyInjection\PublicServicePass('typo3.middleware'));
     $containerBuilder->addCompilerPass(new DependencyInjection\PublicServicePass('typo3.request_handler'));
     $containerBuilder->addCompilerPass(new DependencyInjection\AutowireInjectMethodsPass());
     $containerBuilder->addCompilerPass(new DependencyInjection\ControllerWithPsr7ActionMethodsPass);
+
+    $containerBuilder->addCompilerPass(new class implements CompilerPassInterface {
+        public function process(ContainerBuilder $container)
+        {
+            // FAL registries
+            $rendererRegistry = $container->findDefinition(Resource\Rendering\RendererRegistry::class);
+            foreach ($container->findTaggedServiceIds('fal.file_renderer') as $id => $tags) {
+                $container->findDefinition($id)->setPublic(true);
+                $rendererRegistry->addMethodCall('registerRendererClass', [$id]);
+            }
+            $extractorRegistry = $container->findDefinition(Resource\Index\ExtractorRegistry::class);
+            foreach ($container->findTaggedServiceIds('fal.extractor') as $id => $tags) {
+                $container->findDefinition($id)->setPublic(true);
+                $extractorRegistry->addMethodCall('registerExtractionService', [$id]);
+            }
+            $textExtractorRegistry = $container->findDefinition(Resource\TextExtraction\TextExtractorRegistry::class);
+            foreach ($container->findTaggedServiceIds('fal.text_extractor') as $id => $tags) {
+                $container->findDefinition($id)->setPublic(true);
+                $textExtractorRegistry->addMethodCall('registerTextExtractor', [$id]);
+            }
+        }
+    });
 
     // Executed *after* all compiler passes of *all* other extensions
     $containerBuilder->addCompilerPass(new DependencyInjection\DefaultToNonSharedPass, PassConfig::TYPE_BEFORE_OPTIMIZATION, -1010);
