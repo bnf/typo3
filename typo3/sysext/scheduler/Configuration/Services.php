b2da97ad406256b0bc6aa65102b419a0ca07c880
Benjamin Franzke <bfr@qbus.de>
2019-01-30 13:44:43 +0100

    [WIP][FEATURE] Configure scheduler tasks using symfony service tags
    Change-Id: I434609478881b49b23a8d621d4e7f40c3080b167

--- typo3/sysext/scheduler/Configuration/Services.php
+++ typo3/sysext/scheduler/Configuration/Services.php
@@ -2,9 +2,33 @@
 declare(strict_types = 1);
 namespace TYPO3\CMS\Scheduler;
 
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use TYPO3\CMS\Scheduler\Controller\SchedulerModuleController;
 
-return function (ContainerConfigurator $configurator) {
+return function (ContainerConfigurator $configurator, ContainerBuilder $container) {
+    $container->addCompilerPass(new class implements CompilerPassInterface {
+        public function process(ContainerBuilder $container)
+        {
+            $schedulerModuleControllerDefinition = $container->findDefinition(SchedulerModuleController::class);
+            if ($schedulerModuleControllerDefinition) {
+                foreach ($container->findTaggedServiceIds('scheduler.task') as $id => $tags) {
+                    $container->findDefinition($id)->setPublic(true);
+                    foreach ($tags as $attributes) {
+                        $schedulerModuleControllerDefinition->addMethodCall('addTask', [
+                            $id,
+                            $attributes['title'],
+                            $attributes['description'] ?? '',
+                            $attributes['extension'] ?? '',
+                            $attributes['additionalFields'] ?? null,
+                        ]);
+                    }
+                }
+            }
+        }
+    });
+
     $configurator = $configurator->services()->defaults()
         ->private()
         ->autoconfigure()
