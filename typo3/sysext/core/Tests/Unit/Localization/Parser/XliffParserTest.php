064ae954fd345ca13f4e9cc17855c030d1ace0b5
Benjamin Franzke <bfr@qbus.de>
2019-02-09 17:44:02 +0100

    [TASK] Define singletons used in install tool in service providers
    Switch to use dependency injection to other singletons. were possible.
    
    Releases: master
    Resolves: #
    Change-Id: Ib72d6440f81b2c0d05279e8768697c3b48aecfe4

--- typo3/sysext/core/Tests/Unit/Localization/Parser/XliffParserTest.php
+++ typo3/sysext/core/Tests/Unit/Localization/Parser/XliffParserTest.php
@@ -18,6 +18,7 @@ use Prophecy\Argument;
 use TYPO3\CMS\Core\Cache\CacheManager;
 use TYPO3\CMS\Core\Cache\Frontend\FrontendInterface;
 use TYPO3\CMS\Core\Core\Environment;
+use TYPO3\CMS\Core\Localization\LanguageStore;
 use TYPO3\CMS\Core\Localization\LocalizationFactory;
 use TYPO3\CMS\Core\Localization\Parser\XliffParser;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
@@ -34,6 +35,11 @@ class XliffParserTest extends UnitTestCase
     protected $xliffFileNames;
 
     /**
+     * @var ObjectProphecy|CacheManager
+     */
+    protected $cacheManagerProphecy;
+
+    /**
      * Prepares the environment before running a test.
      */
     protected function setUp(): void
@@ -48,10 +54,10 @@ class XliffParserTest extends UnitTestCase
         ];
         $GLOBALS['TYPO3_CONF_VARS']['SYS']['lang']['format']['priority'] = 'xlf';
 
-        $cacheManagerProphecy = $this->prophesize(CacheManager::class);
-        GeneralUtility::setSingletonInstance(CacheManager::class, $cacheManagerProphecy->reveal());
+        $this->languageStoreProphecy = $this->prophesize(LanguageStore::class);
+        $this->cacheManagerProphecy = $this->prophesize(CacheManager::class);
         $cacheFrontendProphecy = $this->prophesize(FrontendInterface::class);
-        $cacheManagerProphecy->getCache('l10n')->willReturn($cacheFrontendProphecy->reveal());
+        $this->cacheManagerProphecy->getCache('l10n')->willReturn($cacheFrontendProphecy->reveal());
         $cacheFrontendProphecy->get(Argument::cetera())->willReturn(false);
         $cacheFrontendProphecy->set(Argument::cetera())->willReturn(null);
         $cacheFrontendProphecy->flush()->willReturn(null);
@@ -60,11 +66,13 @@ class XliffParserTest extends UnitTestCase
     /**
      * Cleans up the environment after running a test.
      */
+    /*
     protected function tearDown(): void
     {
         GeneralUtility::purgeInstances();
         parent::tearDown();
     }
+     */
 
     /**
      * @test
@@ -106,7 +114,7 @@ class XliffParserTest extends UnitTestCase
     public function canOverrideXliff()
     {
         /** @var $factory LocalizationFactory */
-        $factory = new LocalizationFactory;
+        $factory = new LocalizationFactory(new LanguageStore(), $this->cacheManagerProphecy->reveal());
 
         $GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride'][$this->xliffFileNames['locallang']][] = $this->xliffFileNames['locallang_override'];
         $GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['fr'][$this->xliffFileNames['locallang']][] = $this->xliffFileNames['locallang_override_fr'];
@@ -143,7 +151,7 @@ class XliffParserTest extends UnitTestCase
      */
     public function canOverrideXliffWithFrenchOnly()
     {
-        $factory = new LocalizationFactory;
+        $factory = new LocalizationFactory(new LanguageStore(), $this->cacheManagerProphecy->reveal());
 
         $GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['fr'][$this->xliffFileNames['locallang']][] = $this->xliffFileNames['locallang_override_fr'];
         $LOCAL_LANG = $factory->getParsedData($this->xliffFileNames['locallang'], 'fr');
