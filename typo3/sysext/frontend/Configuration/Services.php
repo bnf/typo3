eef7f79d7019d6dbc1d73655a57a017f548b3c51
Benjamin Franzke <bfr@qbus.de>
2019-02-15 20:26:10 +0100

    [WIP] Migrate from Services.yaml to Service.php only configuration
    Change-Id: I7c6fc5ba37992ad4a3fb5edd3b83280c8e83a5ca

---
+++ typo3/sysext/frontend/Configuration/Services.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types = 1);
+namespace TYPO3\CMS\Frontend;
+
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return function (ContainerConfigurator $configurator) {
+    $configurator = $configurator->services()->defaults()
+        ->private()
+        ->autoconfigure()
+        ->autowire();
+
+    $configurator
+        ->load(__NAMESPACE__ . '\\', '../Classes/*');
+
+    $configurator->set(Controller\TypoScriptFrontendController::class)
+        ->factory([Controller\TypoScriptFrontendController::class, 'getGlobalInstance'])
+        ->share(false)
+        ->autoconfigure(false)
+        ->autowire(false);
+
+    $configurator->set(ContentObject\ContentObjectRenderer::class)
+        ->share(false)
+        ->public();
+};
