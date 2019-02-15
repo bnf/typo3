ff4b48283ee622b5d1068c1f63993911cdab1f59
Benjamin Franzke <bfr@qbus.de>
2019-02-15 20:26:10 +0100

    [WIP] Migrate from Services.yaml to Service.php only configuration
    Change-Id: I7c6fc5ba37992ad4a3fb5edd3b83280c8e83a5ca

---
+++ typo3/sysext/reports/Configuration/Services.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types = 1);
+namespace TYPO3\CMS\Reports;
+
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return function (ContainerConfigurator $configurator) {
+    $configurator = $configurator->services()->defaults()
+        ->private()
+        ->autoconfigure()
+        ->autowire();
+
+    $configurator
+        ->load(__NAMESPACE__ . '\\', '../Classes/*');
+
+    $configurator->set(Controller\ReportController::class)
+        ->tag('backend.module_controller');
+};
