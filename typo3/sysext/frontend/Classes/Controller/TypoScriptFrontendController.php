8c982a8906e469004cb7dd67f04f7cff26662cc7
Benjamin Franzke <bfr@qbus.de>
2019-02-23 23:19:10 +0100

    [WIP] Inject container non nuullable into ContentObjectRenderer
    Change-Id: I2903b5a32cdd8d9eeb604678b77c8e7fb8502475

--- typo3/sysext/frontend/Classes/Controller/TypoScriptFrontendController.php
+++ typo3/sysext/frontend/Classes/Controller/TypoScriptFrontendController.php
@@ -3043,7 +3043,7 @@ class TypoScriptFrontendController implements LoggerAwareInterface
      */
     public function newCObj()
     {
-        $this->cObj = GeneralUtility::makeInstance(ContentObjectRenderer::class, $this);
+        $this->cObj = new ContentObjectRenderer(GeneralUtility::getContainer(), $this);
         $this->cObj->start($this->page, 'pages');
     }
 
