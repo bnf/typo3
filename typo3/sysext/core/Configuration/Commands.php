bd71dd2743f96ea1680568a11e069cb57d5974c5
Benjamin Franzke <bfr@qbus.de>
2019-07-04 13:30:10 +0200

    [FEATURE] Add DI cache warmup/services plot command
    This adds to commands:
    
     * A cache:warmup CLI command: Currently warm ups DI configuration only;
       will be extended to be able to warm up other caches in future.
     * A services:plot CLI command to visualize the service dependencies as
       dot(1) diagram
    
    Resolves: #
    Releases: master

--- typo3/sysext/core/Configuration/Commands.php
+++ typo3/sysext/core/Configuration/Commands.php
@@ -5,6 +5,14 @@ return [
         'class' => \TYPO3\CMS\Core\Command\DumpAutoloadCommand::class,
         'schedulable' => false,
     ],
+    'cache:warmup' => [
+        'class' => \TYPO3\CMS\Core\Command\CacheWarmupCommand::class,
+        'schedulable' => false,
+    ],
+    'services:plot' => [
+        'class' => \TYPO3\CMS\Core\Command\ServicesPlotCommand::class,
+        'schedulable' => false,
+    ],
     'mailer:spool:send' => [
         'class' => \TYPO3\CMS\Core\Command\SendEmailCommand::class,
     ],
