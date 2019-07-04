8ad8df5134ef383af069e3b170194b0f08a1560b
Benjamin Franzke <bfr@qbus.de>
2019-07-04 13:30:10 +0200

    [FEATURE] Add DI cache warmup/services plot command
    This adds to commands:
    
     * A cache:warmup CLI command: Currently warm ups DI configuration only;
       will be extended to be able to warm up other caches in future.
     * A services:plot CLI command to visualize the service dependencies as
       dot(1) diagram
    
    Resolves: #
    Releases: master
    Change-Id: I1dd8ee910f1419b640b9cf4c61192cbb5f65b142

---
+++ typo3/sysext/core/Classes/Command/ServicesPlotCommand.php
@@ -0,0 +1,62 @@
+<?php
+declare(strict_types = 1);
+namespace TYPO3\CMS\Core\Command;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use TYPO3\CMS\Core\Core\Bootstrap;
+use TYPO3\CMS\Core\DependencyInjection\ContainerBuilder;
+use TYPO3\CMS\Core\Package\PackageManager;
+
+/**
+ * Command for dumping a man:dot(1) plot of class dependencies.
+ */
+class ServicesPlotCommand extends Command
+{
+    /**
+     * @var
+     */
+    protected $containerBuilder;
+
+    public function __construct(ContainerBuilder $containerBuilder)
+    {
+        $this->containerBuilder = $containerBuilder;
+        parent::__construct();
+    }
+
+    /**
+     * Defines the allowed options for this command
+     */
+    protected function configure()
+    {
+        $this->setDescription('Output dependency injection graph in man:dot(1) format');
+        $this->setHelp('This command plots a dependency graph that can be converted into svg using `dot -Tsvg`');
+    }
+
+    /**
+     * Dumps the class loading information
+     *
+     * @inheritdoc
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $coreCache = Bootstrap::createCache('core', false);
+        $packageManager = Bootstrap::createPackageManager(PackageManager::class, $coreCache);
+
+        $output->write($this->containerBuilder->plotContainer($packageManager));
+    }
+}
