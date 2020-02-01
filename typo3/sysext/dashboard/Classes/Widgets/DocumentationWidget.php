c917ea4cfd64c9f5531980ca8daf00a76a374bbc
Richard Haeser <richard@maxserv.com>
2020-02-01 15:58:07 +0100

    [FEATURE] Dashboard for TYPO3
    A dashboard is introduced into TYPO3 to show the most important
    information to the current logged in user.
    
    Every user with access to this backend module can now have one
    or more personal dashboards. Each dashboard can contain several
    widgets. Which widgets and in which order the widgets are shown
    is up to the users themselves.
    
    Resolves: #90333
    Releases: master
    Change-Id: I964a52846ec9a1c901baa89508b9df0caecdf81f

---
+++ typo3/sysext/dashboard/Classes/Widgets/DocumentationWidget.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types = 1);
+namespace TYPO3\CMS\Dashboard\Widgets;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+/**
+ * This widget will show a link to the documentation of TYPO3 to make it easier for people
+ * to find the right documentation
+ */
+class DocumentationWidget extends AbstractCtaButtonWidget
+{
+    protected $title = 'LLL:EXT:dashboard/Resources/Private/Language/locallang.xlf:widgets.documentation.title';
+    protected $description = 'LLL:EXT:dashboard/Resources/Private/Language/locallang.xlf:widgets.documentation.description';
+    protected $label = 'LLL:EXT:dashboard/Resources/Private/Language/locallang.xlf:widgets.documentation.content.label';
+    protected $link = 'https://docs.typo3.org';
+    protected $iconIdentifier = 'dashboard-documentation';
+}
