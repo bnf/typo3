540bed8564a5a72c9e12ff6847e9ebf53bae36ba
Benjamin Franzke <bfr@qbus.de>
2020-06-04 07:50:07 +0200

    tmp
    Change-Id: I55ab95599e53c87613ead015b9b98bead5bcc962

--- Build/Sources/TypeScript/backend/Tests/Element/ImmediateActionElementTest.ts
+++ Build/Sources/TypeScript/backend/Tests/Element/ImmediateActionElementTest.ts
@@ -15,68 +15,67 @@ import ImmediateActionElement = require('TYPO3/CMS/Backend/Element/ImmediateActi
 import moduleMenuApp = require('TYPO3/CMS/Backend/ModuleMenu');
 import viewportObject = require('TYPO3/CMS/Backend/Viewport');
 
-function waitForComponentToRender(tag: string) {
-  return new Promise<HTMLElement>((resolve: Function) => {
-    function requestComponent() {
-      const element = document.querySelector(tag);
-      if (element) {
-        resolve(element);
-      } else {
-        window.requestAnimationFrame(requestComponent);
-      }
-    }
-    requestComponent();
-  });
-}
+// Setup the test harness. This will get cleaned out with every test.
+const container = document.createElement('div');
+document.body.appendChild(container);
+let root: HTMLElement; // This will hold the actual element under test.
 
 describe('TYPO3/CMS/Backend/Element/ImmediateActionElement:', () => {
-  it('dispatches action when created via createElement', () => {
+
+  beforeEach((): void => {
+    root = document.createElement('div');
+    container.appendChild(root);
+  });
+
+  afterEach((): void => {
+    container.innerHTML = '';
+    root = null;
+  });
+
+  it('dispatches action when created via createElement', async () => {
     const observer = {
       callback: (): void => {
         return;
       },
     };
     spyOn(observer, 'callback').and.callThrough();
-
     viewportObject.Topbar.refresh = observer.callback;
     const element = <ImmediateActionElement>document.createElement('typo3-immediate-action');
     element.setAttribute('action', 'TYPO3.Backend.Topbar.refresh');
     expect(observer.callback).not.toHaveBeenCalled();
 
-    document.body.appendChild(element).remove();
-    waitForComponentToRender(ImmediateActionElement.tag).then(() => {
-      expect(observer.callback).toHaveBeenCalled();
-    });
+    root.appendChild(element);
+    await new Promise(window.requestAnimationFrame);
+    expect(observer.callback).toHaveBeenCalled();
   });
 
-  it('dispatches action when created from string', () => {
+  it('dispatches action when created from string', async () => {
     const observer = {
       callback: (): void => {
         return;
       },
     };
     spyOn(observer, 'callback').and.callThrough();
-
     moduleMenuApp.App.refreshMenu = observer.callback;
     const element = document.createRange().createContextualFragment('<typo3-immediate-action action="TYPO3.ModuleMenu.App.refreshMenu"></typo3-immediate-action>').querySelector('typo3-immediate-action');
     expect(observer.callback).not.toHaveBeenCalled();
-    document.body.appendChild(element).remove();
-    waitForComponentToRender(ImmediateActionElement.tag).then(() => {
-      expect(observer.callback).toHaveBeenCalled();
-    });
+
+    root.appendChild(element);
+    await new Promise(window.requestAnimationFrame);
+    expect(observer.callback).toHaveBeenCalled();
   });
 
-  it('dispatches action when created via innerHTML', () => {
+  it('dispatches action when created via innerHTML', async () => {
     const observer = {
       callback: (): void => {
         return;
       },
     };
+    spyOn(observer, 'callback').and.callThrough();
     moduleMenuApp.App.refreshMenu = observer.callback;
-    document.body.innerHTML += '<typo3-immediate-action action="TYPO3.ModuleMenu.App.refreshMenu"></typo3-immediate-action>';
-    waitForComponentToRender(ImmediateActionElement.tag).then((element: HTMLElement) => {
-      expect(observer.callback).toHaveBeenCalled();
-      element.remove();
-    });
+
+    root.innerHTML += '<typo3-immediate-action action="TYPO3.ModuleMenu.App.refreshMenu"></typo3-immediate-action>';
+    await new Promise(window.requestAnimationFrame);
+    expect(observer.callback).toHaveBeenCalled();
   });
 });
