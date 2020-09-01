5a8803d3c5d26ae7b05d940a1624d9b23a2255b8
Benjamin Franzke <bfr@qbus.de>
2020-09-01 22:05:15 +0200

    [TASK] Adapt paths in Build/Sources/TypeScript to reflect webpath structure
    The path structure originates back from the days where TypeScript
    sources we included in typo3/sysext/Resources/Public/TypeScript.
    Now that TypeScript files are located in Build/Sources, there is no need
    to rewrite the paths from TypeScript during compilation.
    
    Executed commands:
      # TODO: fix this command
      find Build/Sources/TypeScript/ \
    	-mindepth 1 -maxdepth 4 -name TypeScript -type d \
    	-exec git mv {} {}/../JavaScript \;
      yarn build
    
    Releases: master, 10.4, 9.5
    Resolves: #
    Related: #88043
    Change-Id: Ib7c65d2a1abacd4d2ac91d83e9cd043391e5ddad

---
+++ Build/Sources/TypeScript/filelist/Resources/Public/JavaScript/FileListLocalisation.ts
@@ -0,0 +1,32 @@
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+import DocumentService from 'TYPO3/CMS/Core/DocumentService';
+import RegularEvent from 'TYPO3/CMS/Core/Event/RegularEvent';
+
+/**
+ * Module: TYPO3/CMS/Filelist/FileListLocalisation
+ * @exports TYPO3/CMS/Filelist/FileListLocalisation
+ */
+class FileListLocalisation {
+  constructor() {
+    DocumentService.ready().then((): void => {
+      new RegularEvent('click', (event: Event, target: HTMLElement): void => {
+        const id = target.dataset.fileid;
+        document.querySelector('div[data-fileid="' + id + '"]').classList.toggle('hidden');
+      }).delegateTo(document, 'a.filelist-translationToggler');
+    });
+  }
+}
+
+export default new FileListLocalisation();
