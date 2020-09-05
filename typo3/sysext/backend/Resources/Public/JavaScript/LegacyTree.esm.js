449a7f641e3897a2442e73ed6620cf2f54d3d8bd
Benjamin Franzke <bfr@qbus.de>
2020-09-05 07:53:11 +0200

    Compile ESM modules into typo3/sysext
    Change-Id: Ia7b565de8ad20d8e05b4fad9d4c34a8741b73589

---
+++ typo3/sysext/backend/Resources/Public/JavaScript/LegacyTree.esm.js
@@ -0,0 +1,158 @@
+import jQuery from '../../../../core/Resources/Public/JavaScript/Contrib/jquery/jquery.esm.js';
+
+DragDrop = {
+  dragID: null,
+  table: null
+};
+DragDrop.dragElement = function (event, $element) {
+  event.preventDefault();
+  var $container = $element.parent().parent();
+  var elementID = $container.prop("id");
+  elementID = elementID.substring(elementID.indexOf("_") + 1);
+  DragDrop.dragID = DragDrop.getIdFromEvent(event);
+  if (!DragDrop.dragID) {
+    return false;
+  }
+  if (!elementID) {
+    elementID = DragDrop.dragID;
+  }
+  if (jQuery("#dragIcon").length === 0) {
+    jQuery("body").append("<div id=\"dragIcon\" style=\"display: none;\">&nbsp;</div>");
+  }
+  jQuery("#dragIcon").html($container.find(".dragIcon").html() + $container.find(".dragTitle").children(":first").text());
+  document.onmouseup = function (event) {
+    DragDrop.cancelDragEvent(event);
+  };
+  document.onmousemove = function (event) {
+    DragDrop.mouseMoveEvent(event);
+  };
+};
+DragDrop.dropElement = function (event) {
+  var dropID = DragDrop.getIdFromEvent(event);
+  if (DragDrop.dragID && DragDrop.dragID !== dropID) {
+    var dragID = DragDrop.dragID;
+    var table = DragDrop.table;
+    var parameters = "table=" + table + "-drag" + "&uid=" + dragID + "&dragDrop=" + table + "&srcId=" + dragID + "&dstId=" + dropID;
+    require(["TYPO3/CMS/Backend/ContextMenu"], function (ContextMenu) {
+      ContextMenu.record = {
+        table: decodeURIComponent(table),
+        uid: decodeURIComponent(dragID)
+      };
+      ContextMenu.fetch(parameters);
+    });
+  }
+  DragDrop.cancelDragEvent();
+  return false;
+};
+DragDrop.cancelDragEvent = function (event) {
+  DragDrop.dragID = null;
+  if (jQuery("#dragIcon").length && jQuery("#dragIcon").is(":visible")) {
+    jQuery("#dragIcon").hide();
+  }
+  document.onmouseup = null;
+  document.onmousemove = null;
+};
+DragDrop.mouseMoveEvent = function (event) {
+  if (!event) {
+    event = window.event;
+  }
+  jQuery("#dragIcon").css({
+    left: event.x + 5 + "px",
+    top: event.y - 5 + "px"
+  }).show();
+};
+DragDrop.getIdFromEvent = function (event) {
+  var obj = event.currentTarget;
+  while (obj.id == false && obj.parentNode) {
+    obj = obj.parentNode;
+  }
+  return obj.id.substring(obj.id.indexOf("_") + 1);
+};
+Tree = {
+  ajaxRoute: "sc_alt_file_navframe_expandtoggle",
+  frameSetModule: null,
+  activateDragDrop: true,
+  highlightClass: "active",
+  pageID: 0,
+  noop: function () {},
+  load: function (params, isExpand, obj, scopeData, scopeHash) {
+    var $obj = jQuery(obj);
+    var $parentNode = jQuery(obj).parent().parent();
+    if (!isExpand) {
+      $parentNode.find("ul:first").remove();
+      var $pm = $obj.parent().find(".pm:first");
+      if ($pm.length) {
+        $pm.get().onclick = null;
+        var src = $pm.children(":first").prop("src");
+        src = src.replace(/minus/, "plus");
+        $pm.children("first").prop("src", src);
+      }
+    } else {
+      $obj.css({
+        cursor: "wait"
+      });
+    }
+    jQuery.ajax({
+      url: TYPO3.settings.ajaxUrls[this.ajaxRoute],
+      data: {
+        PM: params,
+        scopeData: scopeData,
+        scopeHash: scopeHash
+      }
+    }).done(function (data) {
+      $parentNode.replaceWith(data);
+      Tree.reSelectActiveItem();
+    });
+  },
+  refresh: function () {
+    var r = new Date();
+    var loc = window.location.href.replace(/&randNum=\d+|#.*/g, "");
+    var addSign = loc.indexOf("?") > 0 ? "&" : "?";
+    window.location = loc + addSign + "randNum=" + r.getTime();
+  },
+  registerDragDropHandlers: function () {
+    if (!Tree.activateDragDrop) {
+      return;
+    }
+    jQuery(".list-tree-root").on("mousedown", ".dragTitle, .dragIcon", function (evt) {
+      DragDrop.dragElement(evt, jQuery(this));
+    }).on("mouseup", ".dragTitle, .dragIcon", function (evt) {
+      DragDrop.dropElement(evt, jQuery(this));
+    });
+  },
+  reSelectActiveItem: function () {
+    if (!top.fsMod) {
+      return;
+    }
+    var $activeItem = jQuery("#" + top.fsMod.navFrameHighlightedID[this.frameSetModule]);
+    if ($activeItem.length) {
+      $activeItem.addClass(Tree.highlightClass);
+      Tree.extractPageIdFromTreeItem($activeItem.prop("id"));
+    }
+  },
+  highlightActiveItem: function (frameSetModule, highlightID) {
+    Tree.frameSetModule = frameSetModule;
+    Tree.extractPageIdFromTreeItem(highlightID);
+    var $obj = jQuery("#" + top.fsMod.navFrameHighlightedID[frameSetModule]);
+    if ($obj.length) {
+      $obj.removeClass(Tree.highlightClass);
+    }
+    top.fsMod.navFrameHighlightedID[frameSetModule] = highlightID;
+    jQuery("#" + highlightID).addClass(Tree.highlightClass);
+  },
+  extractPageIdFromTreeItem: function (highlightID) {
+    if (highlightID) {
+      Tree.pageID = highlightID.split("_")[0].substring(5);
+    }
+  }
+};
+document.addEventListener("typo3:filelist:treeUpdateRequested", evt => {
+  var identifier = evt.detail.payload.identifier;
+  if (top.fsMod && top.fsMod.currentBank) {
+    identifier += "_" + top.fsMod.currentBank;
+  }
+  Tree.highlightActiveItem("file", identifier);
+});
+var Tree$1 = Tree;
+
+export default Tree$1;
---
+++ typo3/sysext/filelist/Resources/Public/JavaScript/FileListLocalisation.esm.js
@@ -0,0 +1,31 @@
+import jQuery from '../../../../core/Resources/Public/JavaScript/Contrib/jquery/jquery.esm.js';
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+/**
+ * Module: TYPO3/CMS/Filelist/FileListLocalisation
+ * @exports TYPO3/CMS/Filelist/FileListLocalisation
+ */
+class FileListLocalisation {
+    constructor() {
+        jQuery(() => {
+            jQuery('a.filelist-translationToggler').on('click', (event) => {
+                const id = jQuery(event.currentTarget).attr('data-fileid');
+                jQuery('div[data-fileid="' + id + '"]').toggle();
+            });
+        });
+    }
+}
+var FileListLocalisation$1 = new FileListLocalisation();
+
+export default FileListLocalisation$1;
