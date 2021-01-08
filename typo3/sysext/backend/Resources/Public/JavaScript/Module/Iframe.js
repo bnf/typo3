d569ae99bf1b85522af2e53f3fc0a9a9fe279842
Benjamin Franzke <bfr@qbus.de>
2021-01-08 13:34:25 +0100

    [POC][WIP][FEATURE] Add support for custom HTML component based backend modules
    As alternative to iframe based backend modules,
    HTML components can now be registered that are
    used to implement modules.
    
    A module consists of a JavaScript module that
    provides an implementation for a custom HTML tag.
    This tag is created by the new <typo3-backend-module-router>
    when the route is opened via ModuleMenu or via deep link.
    
    The <typo3-backend-module-router> tries to mimic
    the former content "list_frame" iframe by providing backwards
    compatibility for the iframe `src` attribute, the
    contentWindow.location property, and (@todo) the load event.
    
    The router currently uses two parameters for it's routing.
    A named module attribute, and a endpoint (currently named src).
    That means, currently it route's mostly by module name, no
    client side URL-routing (yet – that's up to research/discussion)
    
    To keep this change rather small, we do not adapt
    all users of ContentContainer/inline javascript
    that acceses top.list_frame. We rather rely on the
    BC layer that typo3-backend-module-router provides,
    to handle these cases for now.
    
    This patch is currently based on (and therefore incorporates changes from):
    https://review.typo3.org/c/Packages/TYPO3.CMS/+/61776/12
    
    Furthermore this patch is a squash of:
    https://github.com/TYPO3/TYPO3.CMS/compare/master...bnf:custom-component-modules-v1
    Therefore, if you want to edit, please send a pull request to the next version:
    https://github.com/bnf/TYPO3.CMS/commits/custom-component-modules-v2
    
    To additionaly include include:
     * <typo3-backend-module> element (alternative for ModuleTemplate docheader)
     * Example adaption of the EXT:lowlevel configuration module as custom
       component module: Demonstrates the frontend rendering advantanges:
       Avoids scroll via buggy anchor links (docheader conflicts), as HTML
       doesn't need to be regenerated when expanding entries.
    
    TODO:
     * Add API allowed modules to push state to browsers history api
     * Maybe drop module attribute from module router
       and just use src for routing (required backend
       api to register JS routes) – would only be used
       to route links to module element. We still need
       backend url generation for tokens (or not?).
     * Reuse module elements when module data is changing
       (Note: changes whether iframe state needs to be
       pushed/replaced)
     * Link based routing (either full client side routing,
       or backend site routing, would then require a data-module
       tag for links)
     * Improve navigation container synchronization (when opening deeplinks)
       Maybe move into the router, or extend the router to
       route the navigation container as well.
       But then router rather becomes `<typo3-backend-application>`
    
    Resolves: #?
    Releases: master
    Change-Id: I682e89649b597c8c74b6a0a8f198f6bcf5bbc347

---
+++ typo3/sysext/backend/Resources/Public/JavaScript/Module/Iframe.js
@@ -0,0 +1,33 @@
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+var __decorate=this&&this.__decorate||function(e,t,o,l){var n,r=arguments.length,c=r<3?t:null===l?l=Object.getOwnPropertyDescriptor(t,o):l;if("object"==typeof Reflect&&"function"==typeof Reflect.decorate)c=Reflect.decorate(e,t,o,l);else for(var d=e.length-1;d>=0;d--)(n=e[d])&&(c=(r<3?n(c):r>3?n(t,o,c):n(t,o))||c);return r>3&&c&&Object.defineProperty(t,o,c),c};define(["require","exports","lit-element","TYPO3/CMS/Core/lit-helper"],(function(e,t,o,l){"use strict";Object.defineProperty(t,"__esModule",{value:!0}),t.IframeModuleElement=void 0;let n=class extends o.LitElement{constructor(){super(...arguments),this.src=""}static get styles(){return o.css`
+      :host {
+        display: block;
+        height: 100%;
+      }
+      iframe {
+        display: block;
+        border: none;
+        height: 100%;
+        width: 1px;
+        min-width: 100%;
+        transform: translate3d(0,0,0);
+      }
+    `}render(){const e=this.src;return console.log("rendering iframe",{src:e}),e?o.html`
+      <iframe
+        src="${e}"
+        title="${l.lll("iframe.listFrame")}"
+        scrolling="no"
+        @load="${this._load}"
+      ></iframe>
+    `:o.html``}connectedCallback(){super.connectedCallback();const e=new CustomEvent("typo3-module-load",{bubbles:!0,composed:!0,detail:{url:this.src,decorate:!1}});this.dispatchEvent(e)}_load(e){const t=e.target;let o=null,l=null;try{o=t.contentWindow.location.href;const e=t.contentDocument.body.querySelector(".module[data-module-name]");l=e&&e.getAttribute("data-module-name")||null,t.contentWindow.addEventListener("unload",e=>{console.log("real iframe unload",e),Promise.resolve().then(()=>{if(null===t.contentWindow)return void console.log("real iframe window not found. we probably got removed.");const e=t.contentWindow.location.href,o=new CustomEvent("typo3-module-load",{bubbles:!0,composed:!0,detail:{url:e,decorate:!0}});this.dispatchEvent(o)})},{once:!0})}catch(e){console.log("iframe catch",e)}console.log("loaded iframe event",e,{url:o,module:l},t===e.target);const n=new CustomEvent("typo3-module-loaded",{bubbles:!0,composed:!0,detail:{url:o,module:l}});console.log("sending out an url change "+o),this.dispatchEvent(n)}};__decorate([o.property({type:String})],n.prototype,"src",void 0),n=__decorate([o.customElement("typo3-iframe-module")],n),t.IframeModuleElement=n}));
