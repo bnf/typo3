549d0e6519778c7a16017fbafee751a61d14404d
Benjamin Franzke <bfr@qbus.de>
2021-01-08 13:34:25 +0100

    [POC][WIP][FEATURE] Add support for custom HTML component based backend modules
    This patch is currently based on (and therefore incorporates changes from):
    https://review.typo3.org/c/Packages/TYPO3.CMS/+/61776/14
    
    Furthermore this patch is a squash of:
    https://github.com/TYPO3/TYPO3.CMS/compare/master...bnf:custom-component-modules-v6
    Therefore, if you want to edit, please send a pull request to the next version:
    https://github.com/bnf/TYPO3.CMS/commits/custom-component-modules-v7
    
    To additionaly include include:
     * <typo3-backend-module> element (alternative for ModuleTemplate docheader)
     * Example adaption of the EXT:lowlevel configuration module as custom
       component module: Demonstrates the frontend rendering advantanges:
       Avoids scroll via buggy anchor links (docheader conflicts), as HTML
       doesn't need to be regenerated when expanding entries and uses <body>
       scrollbar to improve accessibility.
    
    As alternative to iframe based backend modules,
    HTML components can now be registered that are
    used to implement modules.
    
    A module consists of a JavaScript module that
    provides an implementation for a custom HTML tag.
    This tag is created by the new <typo3-backend-module-router>
    when the route is opened via ModuleMenu or via deep link.
    
    The router currently uses two parameters for it's routing.
    A named module attribute, and a endpoint (currently named src).
    That means, currently it route's mostly by module name, no
    client side URL-routing (yet – that's up to research/discussion)
    
    State is managed via the traditional iframe (instead of history.pushState).
    That may looks very strange, but we cannot combine implicit iframe
    history state changes with history.pushState – iframes will take
    precedence. Therefore we have to stick with iframe-state handling.
    The advantage is: All "tranditional" modules will preserve the
    state handling as before.
    
    To keep this change rather small, we do not adapt
    all users of ContentContainer/inline javascript
    that acceses top.list_frame. We rather rely on iframe
    events handlers in order to synchronize the router.
    
    TODO:
     * Rename src-attribute to endpoint (or similar). src
       was needed in a prior patchset, in order to provide
       iframe compat, but iframe compat is now enabled
       by not hiding the iframe in
     * Enhance API to allow modules to push and/or replace state
     * Link based routing (either full client side routing,
       or backend site routing, would then require a data-module
       tag for links)
     * Improve navigation container synchronization (when opening deeplinks)
       Maybe move into the router, or extend the router to
       route the navigation container as well.
    
    Resolves: #?
    Releases: master
    Change-Id: I682e89649b597c8c74b6a0a8f198f6bcf5bbc347

---
+++ Build/Sources/TypeScript/backend/Resources/Public/TypeScript/Module/Iframe.ts
@@ -0,0 +1,151 @@
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+import {html, css, customElement, property, LitElement, TemplateResult, CSSResult} from 'lit-element';
+import {lll} from 'TYPO3/CMS/Core/lit-helper';
+
+/**
+ * Module: TYPO3/CMS/Backend/Module/Iframe
+ */
+@customElement('typo3-iframe-module')
+export class IframeModuleElement extends LitElement {
+  @property({type: String}) src: string = '';
+
+  public static get styles(): CSSResult
+  {
+    // @todo: css is currently unused, as we are not yet using shadow root (because of acceptance tests)
+    return css`
+      :host {
+        display: block;
+        height: 100%;
+      }
+      iframe {
+        display: block;
+        border: none;
+        height: 100%;
+        width: 1px;
+        min-width: 100%;
+        transform: translate3d(0,0,0);
+      }
+    `;
+  }
+
+  public createRenderRoot(): HTMLElement | ShadowRoot {
+    // Need to avoid shadowRoot for now, because of backwoods
+    // acceptance tests via codeception -___-
+    // Will switch to shadow root, as soon acceptance tests
+    // can locate iframes inside shadowRoot
+    return this;
+  }
+
+  public render(): TemplateResult {
+    const src = this.src;
+
+    console.log('rendering iframe', {src});
+
+    if (!src) {
+      return html``;
+    }
+
+    // @todo: class is only set as long as we do not use shadow root
+    return html`
+      <iframe
+        name="list_frame"
+        id="typo3-contentIframe"
+        class="scaffold-content-module-iframe t3js-scaffold-content-module-iframe"
+        src="${src}"
+        title="${lll('iframe.listFrame')}"
+        scrolling="no"
+        @load="${this._load}"
+      ></iframe>
+    `;
+  }
+
+  public connectedCallback(): void {
+    super.connectedCallback();
+    const event = new CustomEvent('typo3-module-load', {
+      bubbles: true,
+      composed: true,
+      detail: {
+        url: this.src,
+        decorate: false
+      }
+    });
+    console.log('connectedCallback', event);
+    //console.error('connectedCallback');
+    this.dispatchEvent(event);
+  }
+
+  public attributeChangedCallback(name: string, oldval: string, newval: string) {
+    super.attributeChangedCallback(name, oldval, newval);
+
+    if (name === 'src') {
+      //this.requestUpdate();
+      const iframe = this.renderRoot.querySelector('iframe');
+      if (iframe) {
+        iframe.contentWindow.location.reload();
+      }
+    }
+  }
+
+  private _load(e: Event) {
+    const iframe = <HTMLIFrameElement> e.target;
+    let url = null;
+    let moduleName = null;
+    try {
+      url = iframe.contentWindow.location.href;
+      const module = iframe.contentDocument.body.querySelector('.module[data-module-name]');
+      moduleName = module ? ( module.getAttribute('data-module-name') || null) : null;
+
+      iframe.contentWindow.addEventListener('unload', (e: Event) => {
+        console.log('real iframe unload', e);
+
+        // Asynchronous execution needed because the URL changes immediately after
+        // the `unload` event is dispatched.
+        new Promise((resolve) => window.setTimeout(resolve, 0)).then(() => {
+        //Promise.resolve().then(() => {
+          if (iframe.contentWindow === null) {
+            console.log('real iframe window not found. we probably got removed.');
+            return;
+          }
+          const url = iframe.contentWindow.location.href;
+          const event = new CustomEvent('typo3-module-load', {
+            bubbles: true,
+            composed: true,
+            detail: {
+              url,
+              // @todo maybe synthetic true/false
+              decorate: true
+            }
+          });
+          this.dispatchEvent(event);
+        });
+      }, { once: true});
+    } catch (e) {
+      console.log('iframe catch', e);
+      // continue
+    }
+
+    console.log('loaded iframe event', e, {url, module: moduleName}, iframe === e.target);
+    const event = new CustomEvent('typo3-module-loaded', {
+      bubbles: true,
+      composed: true,
+      detail: {
+        url,
+        module: moduleName
+      }
+    });
+    console.log('sending out an url change ' + url);
+    this.dispatchEvent(event);
+  }
+}
