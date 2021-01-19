5c42397df8609aea10df290211ed5e53f58a984c
Benjamin Franzke <bfr@qbus.de>
2021-01-19 01:36:33 +0100

    [WIP][TASK] Add <typo3-backend-module> custom element
    Intended to be used as alternative for
    ModuleTemplate in custom element based
    modules.
    
    Resolves: #?
    Releases: master
    Change-Id: Ieaf2655acd66174b00cf8e13904fa05de7f566c9

---
+++ typo3/sysext/backend/Resources/Public/JavaScript/Element/Module.js
@@ -0,0 +1,80 @@
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+var __decorate=this&&this.__decorate||function(e,o,t,d){var r,i=arguments.length,l=i<3?o:null===d?d=Object.getOwnPropertyDescriptor(o,t):d;if("object"==typeof Reflect&&"function"==typeof Reflect.decorate)l=Reflect.decorate(e,o,t,d);else for(var a=e.length-1;a>=0;a--)(r=e[a])&&(l=(i<3?r(l):i>3?r(o,t,l):r(o,t))||l);return i>3&&l&&Object.defineProperty(o,t,l),l};define(["require","exports","lit-element"],(function(e,o,t){"use strict";Object.defineProperty(o,"__esModule",{value:!0}),o.ModuleElement=void 0;let d=class extends t.LitElement{constructor(){super(...arguments),this.size="small"}static get styles(){return t.css`
+      :host {
+        display: block;
+        height: 100%;
+      }
+      .module-docheader {
+        position: sticky;
+        top: 20px;
+        width: 100%;
+        min-height: 65px;
+        z-index: 300;
+        background-color: #eee;
+        border-bottom: 1px solid #c3c3c3;
+        padding: 0 24px;
+        box-sizing: border-box;
+        transition: margin-top .3s ease-in-out;
+      }
+      .module-docheader-bar {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: space-between;
+      }
+
+      .module-docheader-bar > * {
+        box-sizing: border-box;
+        min-height: 26px;
+        margin: 4px 0;
+        line-height: 26px;
+      }
+      .module-docheader-bar-navigation {
+        min-height: 26px;
+      }
+      .module-docheader-bar > *:empty {
+        display: none;
+      }
+
+      .module-docheader-bar-buttons-column-left,
+      .module-docheader-bar-buttons-column-right {
+        display: flex;
+        flex-direction: row;
+      }
+
+      .module-body {
+        padding: 24px 24px;
+      }
+    `}render(){return t.html`
+      <div class="module">
+        <div class="module-loading-indicator"></div>
+
+        <div class="module-docheader">
+          <div class="module-docheader-bar module-docheader-bar-navigation">
+            <slot name="docheader"/>
+          </div>
+          <div class="module-docheader-bar module-docheader-bar-buttons">
+            <div class="module-docheader-bar-buttons-column-left">
+              <slot name="docheader-button-left"></slot>
+            </div>
+            <div class="module-docheader-bar-buttons-column-right">
+              <slot name="docheader-button-right"></slot>
+            </div>
+          </div>
+        </div>
+        <div class="module-body">
+          <slot></slot>
+        </div>
+      </div>
+    `}};__decorate([t.property({type:String})],d.prototype,"size",void 0),d=__decorate([t.customElement("typo3-backend-module")],d),o.ModuleElement=d}));
