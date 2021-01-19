d14970a4cac5f0fea0cd02ad54e9b244ec17ae82
Benjamin Franzke <bfr@qbus.de>
2021-01-19 13:05:13 +0100

    [WIP][TASK] Implement ConfigurationModule as custom element
    Resolves: #?
    Releases: master
    Change-Id: I0f98ca2be9cec8dfc6438be0ae6c4c0387823918

---
+++ Build/Sources/TypeScript/lowlevel/Resources/Public/TypeScript/ConfigurationModule.ts
@@ -0,0 +1,239 @@
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+import {html, css, LitElement, TemplateResult, PropertyValues} from 'lit-element';
+import {customElement, property, state} from 'lit/decorators';
+import {until} from 'lit/directives/until';
+import {ifDefined} from 'lit/directives/if-defined';
+import {live} from 'lit/directives/live';
+import {repeat} from 'lit/directives/repeat';
+import {unsafeHTML} from 'lit/directives/unsafe-html';
+import AjaxRequest = require('TYPO3/CMS/Core/Ajax/AjaxRequest');
+import {AjaxResponse} from 'TYPO3/CMS/Core/Ajax/AjaxResponse';
+
+import Loader = require('TYPO3/CMS/Backend/Viewport/Loader');
+import {ModuleState} from 'TYPO3/CMS/Backend/Module/State';
+
+import 'TYPO3/CMS/Backend/Element/Module';
+import 'TYPO3/CMS/Backend/Element/ButtonElement';
+import 'TYPO3/CMS/Backend/Element/SpinnerElement';
+
+export const componentName = 'typo3-lowlevel-configuration-module';
+
+/**
+ * Module: TYPO3/CMS/Lowlevel/ConfigurationModule
+ */
+@customElement(componentName)
+export class ConfigurationModule extends LitElement {
+  @property({type: String, reflect: true}) endpoint: string = '';
+  @property({type: String, reflect: true}) search: string = '';
+  @property({type: Boolean, reflect: true}) regex: boolean = false;
+  @property({type: Boolean}) active: boolean = false;
+
+  @state() data: any = null;
+  @state() loading: boolean = false;
+  @state() load: boolean = false;
+
+  public static styles = css`
+    :host {
+      display: block;
+      height: 100%;
+    }
+  `;
+
+  public createRenderRoot(): HTMLElement | ShadowRoot {
+    // Avoid shadowRoot for now, to allow css classes to work
+    return this;
+  }
+
+  public render(): TemplateResult {
+    if ((this.data === null || this.load) && !this.loading) {
+      this.loadData();
+    }
+
+    if (!this.data) {
+      return html`
+        <typo3-backend-module>
+          <typo3-backend-spinner slot="docheader-button-left"></typo3-backend-spinner>
+        </typo3-backend-module>
+      `;
+    }
+
+    return html`
+      <typo3-backend-module>
+        <h1>${this.data.labels.configuration}</h1>
+        ${this.renderData()}
+      </typo3-backend-module>
+    `;
+  }
+
+  public shouldUpdate(changedProperties: PropertyValues): boolean {
+    changedProperties.forEach((oldValue, propName) => {
+      if (propName === 'endpoint') {
+        this.load = true;
+      }
+      if (propName === 'active' && oldValue !== true) {
+        this.load = true;
+      }
+    });
+    return this.active;
+  }
+
+  public updated(changedProperties: PropertyValues): void {
+    const url = this.endpoint;
+    const module = 'system_config';
+    console.log('config updated', changedProperties);
+    let sendLoadEvent = false;
+    changedProperties.forEach((oldValue, propName) => {
+      if (propName === 'active' && oldValue !== true) {
+        sendLoadEvent = true;
+      }
+
+      if (propName === 'endpoint') {
+        sendLoadEvent = true;
+      }
+
+      if (propName === 'loading' && oldValue !== true) {
+        Loader.start();
+      }
+      if (propName === 'loading' && oldValue === true) {
+        // @todo use event, together with a counter in module-router
+        Loader.finish();
+        const event = new CustomEvent<ModuleState>('typo3-module-loaded', {
+          bubbles: true,
+          composed: true,
+          detail: {
+            url,
+            module,
+            title: 'Test'
+          }
+        });
+
+        console.log('sending out config module loaded ' + url);
+        this.dispatchEvent(event);
+      }
+    });
+
+    if (sendLoadEvent) {
+      const event = new CustomEvent<ModuleState>('typo3-module-load', {
+        bubbles: true,
+        composed: true,
+        detail: {
+          url,
+          module
+        }
+      });
+      this.dispatchEvent(event);
+    }
+  }
+
+  private async loadData(): Promise<any> {
+    let url = this.endpoint;
+    if (this.search) {
+      url += '&searchString=' + encodeURIComponent(this.search) + '&regexSearch=' + (this.regex ? 1 : 0);
+    }
+
+    this.loading = true;
+    const reponse = await new AjaxRequest(url).get({cache: 'no-cache'});
+    const data = await reponse.resolve();
+
+    this.loading = false;
+    this.load = false;
+    this.data = data;
+  }
+
+  private renderData(): TemplateResult {
+    const data = this.data;
+    const labels = this.data.labels;
+
+    return html`
+      <span slot="docheader">
+        <select @change="${({target}: {target: HTMLSelectElement}) => this.endpoint = target.options[target.selectedIndex].value}">
+          ${repeat(data.items, (item: any) => item.url, (item: any) => html`<option value="${item.url}" selected="${ifDefined(item.active ? true : undefined)}">${item.label}</option>`)}
+        </select>
+      </span>
+      <span slot="docheader">Path info</span>
+      <typo3-backend-button slot="docheader-button-left">Left</typo3-backend-button>
+      <typo3-backend-button slot="docheader-button-right">Right</typo3-backend-button>
+      ${this.loading ? html`<typo3-backend-spinner slot="docheader-button-left"></typo3-backend-spinner>` : ''}
+
+      <h2>${data.treeName}</h2>
+      <div id="lowlevel-config">
+        <form @submit="${this.handleSubmit}">
+          <div class="form-group">
+              <label for="lowlevel-searchString">${labels.enterSearchPhrase}</label>
+              <input class="form-control" type="search" id="lowlevel-searchString" name="searchString" .value="${data.searchString}" />
+          </div>
+          <div class="form-group">
+              <div class="checkbox">
+                  <label for="lowlevel-regexSearch">
+                      <input
+                          type="checkbox"
+                          class="checkbox"
+                          name="regexSearch"
+                          id="lowlevel-regexSearch"
+                          value="1"
+                          checked="${ifDefined(data.regexSearch ? 'checked' : undefined)}"
+                      >
+                      ${labels.useRegExp}
+                  </label>
+              </div>
+          </div>
+          <div class="form-group">
+              <input class="btn btn-default" type="submit" name="search" id="search" value="${labels.search}"/>
+          </div>
+      </div>
+
+      <div>
+        ${this.renderTree(data.treeData)}
+      </div>
+    `;
+  }
+
+  private handleSubmit(e: Event) {
+    e.preventDefault();
+    const searchstring = (<HTMLInputElement>this.querySelector('input[type="search"]')).value;
+    const regexsearch = (<HTMLInputElement>this.querySelector('input[type="checkbox"][name="regexSearch"]')).checked;
+
+    this.search = searchstring ? searchstring : null;
+    this.regex = regexsearch;
+    this.load = true;
+  }
+
+  private renderTree(tree: any): TemplateResult {
+    return html`
+      <ul class="list-tree monospace">
+        ${tree.map((element: any) => this.renderElement(element))}
+      </ul>
+    `;
+  }
+
+  private renderElement(element: any): TemplateResult {
+    return html`
+      <li class="${element.active ? 'active' : ''}">
+        ${element.expandable ? html`<a class="list-tree-control ${element.expanded ? 'list-tree-control-open' : 'list-tree-control-closed'}" id="${element.id}" href="${element.toggle}" @click="${this._linkClick}"><i class="fa"></i></a>` : ''}
+        <span class="list-tree-label">${element.label}</span>
+        ${element.value ? html` = <span class="list-tree-value">${element.value}</span>`: ''}
+        ${element.expanded ? this.renderTree(element.children) : ''}
+      </li>
+    `;
+  }
+
+  private _linkClick(e: Event) {
+    e.preventDefault()
+    const href = (e.target as HTMLElement).getAttribute('href');
+    this.endpoint = href;
+    this.removeAttribute('search');
+    this.removeAttribute('regex');
+  }
+}
---
+++ typo3/sysext/lowlevel/Resources/Public/JavaScript/ConfigurationModule.js
@@ -0,0 +1,79 @@
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+var __decorate=this&&this.__decorate||function(e,t,o,a){var i,l=arguments.length,n=l<3?t:null===a?a=Object.getOwnPropertyDescriptor(t,o):a;if("object"==typeof Reflect&&"function"==typeof Reflect.decorate)n=Reflect.decorate(e,t,o,a);else for(var r=e.length-1;r>=0;r--)(i=e[r])&&(n=(l<3?i(n):l>3?i(t,o,n):i(t,o))||n);return l>3&&n&&Object.defineProperty(t,o,n),n};define(["require","exports","lit-element","lit/decorators","lit/directives/if-defined","lit/directives/repeat","TYPO3/CMS/Core/Ajax/AjaxRequest","TYPO3/CMS/Backend/Viewport/Loader","TYPO3/CMS/Backend/Element/Module","TYPO3/CMS/Backend/Element/ButtonElement","TYPO3/CMS/Backend/Element/SpinnerElement"],(function(e,t,o,a,i,l,n,r){"use strict";Object.defineProperty(t,"__esModule",{value:!0}),t.ConfigurationModule=t.componentName=void 0,t.componentName="typo3-lowlevel-configuration-module";let s=class extends o.LitElement{constructor(){super(...arguments),this.endpoint="",this.search="",this.regex=!1,this.active=!1,this.data=null,this.loading=!1,this.load=!1}createRenderRoot(){return this}render(){return null!==this.data&&!this.load||this.loading||this.loadData(),this.data?o.html`
+      <typo3-backend-module>
+        <h1>${this.data.labels.configuration}</h1>
+        ${this.renderData()}
+      </typo3-backend-module>
+    `:o.html`
+        <typo3-backend-module>
+          <typo3-backend-spinner slot="docheader-button-left"></typo3-backend-spinner>
+        </typo3-backend-module>
+      `}shouldUpdate(e){return e.forEach((e,t)=>{"endpoint"===t&&(this.load=!0),"active"===t&&!0!==e&&(this.load=!0)}),this.active}updated(e){const t=this.endpoint;console.log("config updated",e);let o=!1;if(e.forEach((e,a)=>{if("active"===a&&!0!==e&&(o=!0),"endpoint"===a&&(o=!0),"loading"===a&&!0!==e&&r.start(),"loading"===a&&!0===e){r.finish();const e=new CustomEvent("typo3-module-loaded",{bubbles:!0,composed:!0,detail:{url:t,module:"system_config",title:"Test"}});console.log("sending out config module loaded "+t),this.dispatchEvent(e)}}),o){const e=new CustomEvent("typo3-module-load",{bubbles:!0,composed:!0,detail:{url:t,module:"system_config"}});this.dispatchEvent(e)}}async loadData(){let e=this.endpoint;this.search&&(e+="&searchString="+encodeURIComponent(this.search)+"&regexSearch="+(this.regex?1:0)),this.loading=!0;const t=await new n(e).get({cache:"no-cache"}),o=await t.resolve();this.loading=!1,this.load=!1,this.data=o}renderData(){const e=this.data,t=this.data.labels;return o.html`
+      <span slot="docheader">
+        <select @change="${({target:e})=>this.endpoint=e.options[e.selectedIndex].value}">
+          ${l.repeat(e.items,e=>e.url,e=>o.html`<option value="${e.url}" selected="${i.ifDefined(!!e.active||void 0)}">${e.label}</option>`)}
+        </select>
+      </span>
+      <span slot="docheader">Path info</span>
+      <typo3-backend-button slot="docheader-button-left">Left</typo3-backend-button>
+      <typo3-backend-button slot="docheader-button-right">Right</typo3-backend-button>
+      ${this.loading?o.html`<typo3-backend-spinner slot="docheader-button-left"></typo3-backend-spinner>`:""}
+
+      <h2>${e.treeName}</h2>
+      <div id="lowlevel-config">
+        <form @submit="${this.handleSubmit}">
+          <div class="form-group">
+              <label for="lowlevel-searchString">${t.enterSearchPhrase}</label>
+              <input class="form-control" type="search" id="lowlevel-searchString" name="searchString" .value="${e.searchString}" />
+          </div>
+          <div class="form-group">
+              <div class="checkbox">
+                  <label for="lowlevel-regexSearch">
+                      <input
+                          type="checkbox"
+                          class="checkbox"
+                          name="regexSearch"
+                          id="lowlevel-regexSearch"
+                          value="1"
+                          checked="${i.ifDefined(e.regexSearch?"checked":void 0)}"
+                      >
+                      ${t.useRegExp}
+                  </label>
+              </div>
+          </div>
+          <div class="form-group">
+              <input class="btn btn-default" type="submit" name="search" id="search" value="${t.search}"/>
+          </div>
+      </div>
+
+      <div>
+        ${this.renderTree(e.treeData)}
+      </div>
+    `}handleSubmit(e){e.preventDefault();const t=this.querySelector('input[type="search"]').value,o=this.querySelector('input[type="checkbox"][name="regexSearch"]').checked;this.search=t||null,this.regex=o,this.load=!0}renderTree(e){return o.html`
+      <ul class="list-tree monospace">
+        ${e.map(e=>this.renderElement(e))}
+      </ul>
+    `}renderElement(e){return o.html`
+      <li class="${e.active?"active":""}">
+        ${e.expandable?o.html`<a class="list-tree-control ${e.expanded?"list-tree-control-open":"list-tree-control-closed"}" id="${e.id}" href="${e.toggle}" @click="${this._linkClick}"><i class="fa"></i></a>`:""}
+        <span class="list-tree-label">${e.label}</span>
+        ${e.value?o.html` = <span class="list-tree-value">${e.value}</span>`:""}
+        ${e.expanded?this.renderTree(e.children):""}
+      </li>
+    `}_linkClick(e){e.preventDefault();const t=e.target.getAttribute("href");this.endpoint=t,this.removeAttribute("search"),this.removeAttribute("regex")}};s.styles=o.css`
+    :host {
+      display: block;
+      height: 100%;
+    }
+  `,__decorate([a.property({type:String,reflect:!0})],s.prototype,"endpoint",void 0),__decorate([a.property({type:String,reflect:!0})],s.prototype,"search",void 0),__decorate([a.property({type:Boolean,reflect:!0})],s.prototype,"regex",void 0),__decorate([a.property({type:Boolean})],s.prototype,"active",void 0),__decorate([a.state()],s.prototype,"data",void 0),__decorate([a.state()],s.prototype,"loading",void 0),__decorate([a.state()],s.prototype,"load",void 0),s=__decorate([a.customElement(t.componentName)],s),t.ConfigurationModule=s}));
