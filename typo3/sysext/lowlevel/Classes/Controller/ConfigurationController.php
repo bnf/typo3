0e62fdfea6685433afd96a8549e72287c644d302
Benjamin Franzke <bfr@qbus.de>
2021-01-19 13:05:13 +0100

    [WIP][TASK] Implement ConfigurationModule as custom element
    Resolves: #?
    Releases: master
    Change-Id: I0f98ca2be9cec8dfc6438be0ae6c4c0387823918

--- typo3/sysext/lowlevel/Classes/Controller/ConfigurationController.php
+++ typo3/sysext/lowlevel/Classes/Controller/ConfigurationController.php
@@ -20,13 +20,11 @@ namespace TYPO3\CMS\Lowlevel\Controller;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use TYPO3\CMS\Backend\Routing\UriBuilder;
-use TYPO3\CMS\Backend\Template\ModuleTemplateFactory;
 use TYPO3\CMS\Backend\View\ArrayBrowser;
 use TYPO3\CMS\Core\Authentication\BackendUserAuthentication;
-use TYPO3\CMS\Core\Http\HtmlResponse;
-use TYPO3\CMS\Core\Page\PageRenderer;
+use TYPO3\CMS\Core\Http\JsonResponse;
+use TYPO3\CMS\Core\Localization\LanguageService;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Fluid\View\StandaloneView;
 use TYPO3\CMS\Lowlevel\ConfigurationModuleProvider\ProviderRegistry;
 
 /**
@@ -36,20 +34,14 @@ use TYPO3\CMS\Lowlevel\ConfigurationModuleProvider\ProviderRegistry;
 class ConfigurationController
 {
     protected ProviderRegistry $configurationProviderRegistry;
-    protected PageRenderer $pageRenderer;
     protected UriBuilder $uriBuilder;
-    protected ModuleTemplateFactory $moduleTemplateFactory;
 
     public function __construct(
         ProviderRegistry $configurationProviderRegistry,
-        PageRenderer $pageRenderer,
-        UriBuilder $uriBuilder,
-        ModuleTemplateFactory $moduleTemplateFactory
+        UriBuilder $uriBuilder
     ) {
         $this->configurationProviderRegistry = $configurationProviderRegistry;
-        $this->pageRenderer = $pageRenderer;
         $this->uriBuilder = $uriBuilder;
-        $this->moduleTemplateFactory = $moduleTemplateFactory;
     }
 
     /**
@@ -63,10 +55,8 @@ class ConfigurationController
      */
     public function mainAction(ServerRequestInterface $request): ResponseInterface
     {
-        $moduleTemplate = $this->moduleTemplateFactory->create($request);
         $backendUser = $this->getBackendUser();
         $queryParams = $request->getQueryParams();
-        $postValues = $request->getParsedBody();
         $moduleState = $backendUser->uc['moduleData']['system_config'] ?? [];
 
         $configurationProviderIdentifier = (string)($queryParams['tree'] ?? $moduleState['tree'] ?? '');
@@ -90,8 +80,8 @@ class ConfigurationController
         $configurationArray = $configurationProvider->getConfiguration();
 
         // Search string given or regex search enabled?
-        $searchString = (string)($postValues['searchString'] ? trim($postValues['searchString']) : '');
-        $moduleState['regexSearch'] = (bool)($postValues['regexSearch'] ?? $moduleState['regexSearch'] ?? false);
+        $searchString = (string)($queryParams['searchString'] ? trim($queryParams['searchString']) : '');
+        $moduleState['regexSearch'] = (bool)($queryParams['regexSearch'] ?? $moduleState['regexSearch'] ?? false);
 
         // Prepare array renderer class, apply search and expand / collapse states
         $arrayBrowser = GeneralUtility::makeInstance(ArrayBrowser::class, $request->getAttribute('route'));
@@ -111,57 +101,56 @@ class ConfigurationController
         $backendUser->uc['moduleData']['system_config'] = $moduleState;
         $backendUser->writeUC();
 
-        // Render main body
-        $view = GeneralUtility::makeInstance(StandaloneView::class);
-        $view->getRequest()->setControllerExtensionName('lowlevel');
-        $view->setTemplatePathAndFilename(GeneralUtility::getFileAbsFileName(
-            'EXT:lowlevel/Resources/Private/Templates/Backend/Configuration.html'
-        ));
-        $view->assignMultiple([
+        $uriBuilder = GeneralUtility::makeInstance(UriBuilder::class);
+        $self = (string)$uriBuilder->buildUriFromRoute('system_config', ['tree' =>  $configurationProviderIdentifier]);
+
+        $shortcut = [
+            'routeIdentifier' => 'system_config',
+            'routeArguments' => json_encode(['tree' => $configurationProviderIdentifier]),
+            'displayName' => $configurationProvider->getLabel()
+        ];
+
+        $items = [];
+        foreach ($this->configurationProviderRegistry->getProviders() as $provider) {
+            $item = [
+                'label' => $provider->getLabel(),
+                'url' => (string)$uriBuilder->buildUriFromRoute('system_config', ['tree' => $provider->getIdentifier()]),
+            ];
+
+            if ($provider === $configurationProvider) {
+                $item['active'] = true;
+            }
+            $items[] = $item;
+        }
+
+        $labels = [
+            'configuration' => '',
+            'enterSearchPhrase' => '',
+            'useRegExp' => '',
+            'search' => '',
+        ];
+        foreach ($labels as $key => $_) {
+            $labels[$key] = $this->getLanguageService()->sL('LLL:EXT:lowlevel/Resources/Private/Language/locallang.xlf:' . $key);
+        }
+        return new JsonResponse([
+            'items' => $items,
+            'self' => $self,
+            'shortcut' => $shortcut,
             'treeName' => $configurationProvider->getLabel(),
             'searchString' => $searchString,
             'regexSearch' => $moduleState['regexSearch'],
-            'tree' => $arrayBrowser->tree($configurationArray, ''),
+            'treeData' => $arrayBrowser->treeData($configurationArray, ''),
+            'labels' => $labels,
         ]);
-
-        // Prepare module setup
-        $moduleTemplate->setContent($view->render());
-        $this->pageRenderer->loadRequireJsModule('TYPO3/CMS/Lowlevel/ConfigurationView');
-
-        // Shortcut in doc header
-        $shortcutButton = $moduleTemplate->getDocHeaderComponent()->getButtonBar()->makeShortcutButton();
-        $shortcutButton
-            ->setRouteIdentifier('system_config')
-            ->setDisplayName($configurationProvider->getLabel())
-            ->setArguments(['tree' => $configurationProviderIdentifier]);
-        $moduleTemplate->getDocHeaderComponent()->getButtonBar()->addButton($shortcutButton);
-
-        // Main drop down in doc header
-        $menu = $moduleTemplate->getDocHeaderComponent()->getMenuRegistry()->makeMenu();
-        $menu->setIdentifier('tree');
-
-        foreach ($this->configurationProviderRegistry->getProviders() as $provider) {
-            $menuItem = $menu->makeMenuItem();
-            $menuItem
-                ->setHref((string)$this->uriBuilder->buildUriFromRoute('system_config', ['tree' => $provider->getIdentifier()]))
-                ->setTitle($provider->getLabel());
-            if ($configurationProvider === $provider) {
-                $menuItem->setActive(true);
-            }
-            $menu->addMenuItem($menuItem);
-        }
-
-        $moduleTemplate->getDocHeaderComponent()->getMenuRegistry()->addMenu($menu);
-
-        return new HtmlResponse($moduleTemplate->renderContent());
     }
 
-    /**
-     * Returns the Backend User
-     * @return BackendUserAuthentication
-     */
     protected function getBackendUser(): BackendUserAuthentication
     {
         return $GLOBALS['BE_USER'];
+    }
+
+    protected function getLanguageService(): LanguageService
+    {
+        return $GLOBALS['LANG'];
     }
 }
