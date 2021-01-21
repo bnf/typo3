fee5d41a07425863277814a5d723452f3ec640d3
Benjamin Franzke <bfr@qbus.de>
2021-01-21 22:26:22 +0100

    config cleanup
    Change-Id: I8161fe1719c6c14a33f360fe212a6a32b3710bbe

--- typo3/sysext/lowlevel/Resources/Public/JavaScript/ConfigurationModule.js
+++ typo3/sysext/lowlevel/Resources/Public/JavaScript/ConfigurationModule.js
@@ -10,24 +10,24 @@
  *
  * The TYPO3 project - inspiring people to share!
  */
-var __decorate=this&&this.__decorate||function(e,t,o,r){var a,l=arguments.length,i=l<3?t:null===r?r=Object.getOwnPropertyDescriptor(t,o):r;if("object"==typeof Reflect&&"function"==typeof Reflect.decorate)i=Reflect.decorate(e,t,o,r);else for(var s=e.length-1;s>=0;s--)(a=e[s])&&(i=(l<3?a(i):l>3?a(t,o,i):a(t,o))||i);return l>3&&i&&Object.defineProperty(t,o,i),i};define(["require","exports","lit-element","lit-html/directives/if-defined","lit-html/directives/repeat","TYPO3/CMS/Core/lit-helper","TYPO3/CMS/Core/Ajax/AjaxRequest","TYPO3/CMS/Backend/Element/Module","TYPO3/CMS/Backend/Element/SpinnerElement"],(function(e,t,o,r,a,l,i){"use strict";Object.defineProperty(t,"__esModule",{value:!0}),t.ConfigurationModule=void 0;let s=class extends o.LitElement{constructor(){super(...arguments),this.src="",this.search="",this.regex=!1,this.active=!1,this.data=null,this.loading=!1,this.load=!1}static get styles(){return o.css`
+var __decorate=this&&this.__decorate||function(e,t,o,r){var l,a=arguments.length,i=a<3?t:null===r?r=Object.getOwnPropertyDescriptor(t,o):r;if("object"==typeof Reflect&&"function"==typeof Reflect.decorate)i=Reflect.decorate(e,t,o,r);else for(var s=e.length-1;s>=0;s--)(l=e[s])&&(i=(a<3?l(i):a>3?l(t,o,i):l(t,o))||i);return a>3&&i&&Object.defineProperty(t,o,i),i};define(["require","exports","lit-element","lit-html/directives/if-defined","lit-html/directives/repeat","TYPO3/CMS/Core/lit-helper","TYPO3/CMS/Core/Ajax/AjaxRequest","TYPO3/CMS/Backend/Viewport/Loader","TYPO3/CMS/Backend/Element/Module","TYPO3/CMS/Backend/Element/SpinnerElement"],(function(e,t,o,r,l,a,i,s){"use strict";Object.defineProperty(t,"__esModule",{value:!0}),t.ConfigurationModule=void 0;let n=class extends o.LitElement{constructor(){super(...arguments),this.src="",this.search="",this.regex=!1,this.active=!1,this.data=null,this.loading=!1,this.load=!1}static get styles(){return o.css`
       :host {
         display: block;
         height: 100%;
       }
     `}createRenderRoot(){return this}render(){return null!==this.data&&!this.load||this.loading||this.loadData(),this.data?o.html`
       <typo3-backend-module>
-        <h1>${l.lll("configuration")}</h1>
+        <h1>${a.lll("configuration")}</h1>
         ${this.renderData()}
       </typo3-backend-module>
     `:o.html`
         <typo3-backend-module>
           <typo3-backend-spinner slot="docheader-button-left"></typo3-backend-spinner>
         </typo3-backend-module>
-      `}connectedCallback(){super.connectedCallback();const e=this.src;if(this.active){const t=new CustomEvent("typo3-module-load",{bubbles:!0,composed:!0,detail:{module:"system_config",url:e,decorate:!1}});console.log("sending out config module load "+e),this.dispatchEvent(t)}this.addEventListener("click",e=>{if(console.log("click",e),e.defaultPrevented||0!==e.button||e.metaKey||e.ctrlKey||e.shiftKey)return;const t=e.composedPath().filter(e=>"A"===e.tagName)[0];if(!t||t.target||t.hasAttribute("download")||"external"===t.getAttribute("rel"))return;const o=t.href;o&&-1===o.indexOf("mailto:")&&(e.preventDefault(),this.setAttribute("src",o),this.removeAttribute("search"),this.removeAttribute("regex"))})}attributeChangedCallback(e,t,o){super.attributeChangedCallback(e,t,o),"src"===e&&(this.load=!0)}shouldUpdate(e){return e.forEach((e,t)=>{"active"===t&&!0!==e&&(this.load=!0)}),this.active}updated(e){const t=this.src;console.error("config updated"),console.log("config updated",e),e.forEach((e,o)=>{if("active"===o&&!1===e){const e=new CustomEvent("typo3-module-load",{bubbles:!0,composed:!0,detail:{module:"system_config",url:t,decorate:!1}});console.log("sending out config module load, because of active attr "+this.getAttribute("src")),this.dispatchEvent(e)}if("loading"===o&&!0===e){const e=new CustomEvent("typo3-module-loaded",{bubbles:!0,composed:!0,detail:{url:t,module:"system_config"}});console.log("sending out config module loaded "+t),this.dispatchEvent(e)}})}async loadData(){let e=this.src;this.search&&(e+="&searchString="+encodeURIComponent(this.search)+"&regexSearch="+(this.regex?1:0)),this.loading=!0;const t=await new i(e).get({cache:"no-cache"}),o=await t.resolve();this.loading=!1,this.load=!1,this.data=o}renderData(){const e=this.data;return o.html`
+      `}attributeChangedCallback(e,t,o){super.attributeChangedCallback(e,t,o),"active"!==e&&(this.load=!0)}shouldUpdate(e){return e.forEach((e,t)=>{"active"===t&&!0!==e&&(this.load=!0)}),this.active}updated(e){const t=this.src;console.log("config updated",e),e.forEach((e,o)=>{if("active"===o&&!0!==e){const e=new CustomEvent("typo3-module-load",{bubbles:!0,composed:!0,detail:{module:"system_config",url:t,decorate:!1}});console.log("sending out config module load, because of active attr "+this.src),this.dispatchEvent(e)}if("loading"===o&&!0!==e&&s.start(),"loading"===o&&!0===e){s.finish();const e=new CustomEvent("typo3-module-loaded",{bubbles:!0,composed:!0,detail:{url:t,module:"system_config"}});console.log("sending out config module loaded "+t),this.dispatchEvent(e)}})}async loadData(){let e=this.src;this.search&&(e+="&searchString="+encodeURIComponent(this.search)+"&regexSearch="+(this.regex?1:0)),this.loading=!0;const t=await new i(e).get({cache:"no-cache"}),o=await t.resolve();this.loading=!1,this.load=!1,this.data=o}renderData(){const e=this.data;return o.html`
       <span slot="docheader">
-        <select @change="${({target:e})=>this.setAttribute("src",e.options[e.selectedIndex].value)}">
-          ${a.repeat(e.items,e=>e.url,e=>o.html`<option value="${e.url}" selected="${r.ifDefined(!!e.active||void 0)}">${e.label}</option>`)}
+        <select @change="${({target:e})=>this.src=e.options[e.selectedIndex].value}">
+          ${l.repeat(e.items,e=>e.url,e=>o.html`<option value="${e.url}" selected="${r.ifDefined(!!e.active||void 0)}">${e.label}</option>`)}
         </select>
       </span>
       <span slot="docheader">Path info</span>
@@ -39,7 +39,7 @@ var __decorate=this&&this.__decorate||function(e,t,o,r){var a,l=arguments.length
       <div id="lowlevel-config">
         <form @submit="${this.handleSubmit}">
           <div class="form-group">
-              <label for="lowlevel-searchString">${l.lll("enterSearchPhrase")}</label>
+              <label for="lowlevel-searchString">${a.lll("enterSearchPhrase")}</label>
               <input class="form-control" type="search" id="lowlevel-searchString" name="searchString" .value="${e.searchString}" />
           </div>
           <div class="form-group">
@@ -53,27 +53,27 @@ var __decorate=this&&this.__decorate||function(e,t,o,r){var a,l=arguments.length
                           value="1"
                           checked="${r.ifDefined(e.regexSearch?"checked":void 0)}"
                       >
-                      ${l.lll("useRegExp")}
+                      ${a.lll("useRegExp")}
                   </label>
               </div>
           </div>
           <div class="form-group">
-              <input class="btn btn-default" type="submit" name="search" id="search" value="${l.lll("search")}"/>
+              <input class="btn btn-default" type="submit" name="search" id="search" value="${a.lll("search")}"/>
           </div>
       </div>
 
       <div class="nowrap">
         ${this.renderTree(e.treeData)}
       </div>
-    `}handleSubmit(e){e.preventDefault();const t=this.querySelector('input[type="search"]').value,o=this.querySelector('input[type="checkbox"][name="regexSearch"]').checked;t?this.setAttribute("search",t):this.removeAttribute("search"),o?this.setAttribute("regex",""):this.removeAttribute("regex")}renderTree(e){return o.html`
+    `}handleSubmit(e){e.preventDefault();const t=this.querySelector('input[type="search"]').value,o=this.querySelector('input[type="checkbox"][name="regexSearch"]').checked;this.search=t||null,this.regex=o}renderTree(e){return o.html`
       <ul class="list-tree monospace">
         ${e.map(e=>this.renderElement(e))}
       </ul>
     `}renderElement(e){return o.html`
       <li class="${e.active?"active":""}">
-        ${e.expandable?o.html`<a class="list-tree-control ${e.expanded?"list-tree-control-open":"list-tree-control-closed"}" id="${e.id}" href="${e.toggle}"><i class="fa"></i></a>`:""}
+        ${e.expandable?o.html`<a class="list-tree-control ${e.expanded?"list-tree-control-open":"list-tree-control-closed"}" id="${e.id}" href="${e.toggle}" @click="${this._linkClick}"><i class="fa"></i></a>`:""}
         <span class="list-tree-label">${e.label}</span>
         ${e.value?o.html` = <span class="list-tree-value">${e.value}</span>`:""}
         ${e.expanded?this.renderTree(e.children):""}
       </li>
-    `}};__decorate([o.property({type:String})],s.prototype,"src",void 0),__decorate([o.property({type:String})],s.prototype,"search",void 0),__decorate([o.property({type:Boolean})],s.prototype,"regex",void 0),__decorate([o.property({type:Boolean})],s.prototype,"active",void 0),__decorate([o.internalProperty()],s.prototype,"data",void 0),__decorate([o.internalProperty()],s.prototype,"loading",void 0),__decorate([o.internalProperty()],s.prototype,"load",void 0),s=__decorate([o.customElement("typo3-lowlevel-configuration-module")],s),t.ConfigurationModule=s}));
+    `}_linkClick(e){e.preventDefault();const t=e.target.getAttribute("href");this.setAttribute("src",t),this.removeAttribute("search"),this.removeAttribute("regex")}};__decorate([o.property({type:String,reflect:!0})],n.prototype,"src",void 0),__decorate([o.property({type:String,reflect:!0})],n.prototype,"search",void 0),__decorate([o.property({type:Boolean,reflect:!0})],n.prototype,"regex",void 0),__decorate([o.property({type:Boolean})],n.prototype,"active",void 0),__decorate([o.internalProperty()],n.prototype,"data",void 0),__decorate([o.internalProperty()],n.prototype,"loading",void 0),__decorate([o.internalProperty()],n.prototype,"load",void 0),n=__decorate([o.customElement("typo3-lowlevel-configuration-module")],n),t.ConfigurationModule=n}));
