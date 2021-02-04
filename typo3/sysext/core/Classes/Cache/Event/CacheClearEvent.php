2766a4c97dcda09c3147cfb3269f6e4a5af6b5b1
Benjamin Franzke <bfr@qbus.de>
2021-02-04 20:19:18 +0100

    [WIP][FEATURE] Add cache:clear command
    TODO:
     * Streamline cache clearance in general
       installtool ClearCache, DataHandler,
       all could just dispatch an event
    
     * Maybe add a context to the event
       for the event subscriber to decide
       whether caches are to be flushed?
    
    Releases: master
    Resolves: #90197
    Change-Id: I08dd8b7b376afe34e1bf747ad1228b29595caa13

---
+++ typo3/sysext/core/Classes/Cache/Event/CacheClearEvent.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace TYPO3\CMS\Core\Cache\Event;
+
+/**
+ * Event fired when caches are to be cleared
+ */
+final class CacheClearEvent
+{
+    private string $group = '';
+    private array $tags = [];
+    private array $errors = [];
+
+    public function __construct(string $group, array $tags)
+    {
+        $this->group = $group;
+        $this->tags = $tags;
+    }
+
+    public function getGroup(): string
+    {
+        return $this->group;
+    }
+
+    public function getTags(): array
+    {
+        return $this->tags;
+    }
+
+    public function getErrors(): array
+    {
+        return $this->errors;
+    }
+
+    public function addError(string $error): void
+    {
+        $this->errors[] = $error;
+    }
+}
