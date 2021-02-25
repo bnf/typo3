a02a0177618641d5ca9d2f7bb785d6d000131ff1
Benjamin Franzke <bfr@qbus.de>
2021-02-25 09:36:25 +0100

    [WIP][TASK] Update to Lit v2
    This patch is currently WIP as the final v2 and v3 version
    are not released yet. Therefore this is using pre-releases for now.
    
    Lit is the umbrella term for the next major
    lit-html (v2) and lit-element (v3) versions,
    and is advertised as separate entry point module named `lit`:
      > The main module exports the core pieces needed for component
      > development: LitElement, html, css, and the most
    
    lit-html v2 and lit-element v3 are mostly compatible
    to the previous major versions. Main changes are
    
     * Deprecations of entry points in favor of
       new `lit` module
     * @internalProperty changed to @state
     * shadow css declaration via static property
       instead of static getter method
    
    Commands used:
    
      rm -rf typo3/sysext/core/Resources/Public/JavaScript/Contrib/lit-{element,html}/
      yarn add lit-html@next-major lit-element@next-major
      yarn add lit@2.0.0-pre.2
      yarn add --dev @rollup/plugin-replace
      yarn build
      git add typo3/sysext/core/Resources/Public/JavaScript/Contrib/{@lit,lit-element,lit-html,lit}/
    
      # todo use upstream 6.x.0 release
      composer config repositories.testing-framework vcs https://github.com/bnf/testing-framework.git
      composer require --dev typo3/testing-framework:^6.9.0@dev
      composer require --dev typo3/testing-framework:^6.9.0@dev \
        --no-update --working-dir=typo3/sysext/core
    
    TODO:
     * Check if bundling lit becomes a possibility through
       the new entry point (it will probably not though)
     * If bundling lit is not possible, and because most of `lit` is just
       an import wrapper for lit-element/html, see if we rather
       configure requirejs aliases for decorators and directives
       (to avoid loading many small wrapper module files)
     * Consider usage of the new `@lit/localize`:
       https://github.com/Polymer/lit-html/tree/lit-next/packages/localize
    
    Resolves: #
    Releases: master
    Change-Id: I9b659d851e6ad9dc3cc649bd40aab886b86fb0f8

---
+++ typo3/sysext/core/Resources/Public/JavaScript/Contrib/lit-html/static.js
@@ -0,0 +1,6 @@
+define(["exports","./lit-html"],(function(exports,litHtml){"use strict";
+/**
+	 * @license
+	 * Copyright 2020 Google LLC
+	 * SPDX-License-Identifier: BSD-3-Clause
+	 */const l=new Map,r=t=>(o,...i)=>{var r;const s=i.length;let n,v;const $=[],a=[];let c,e=0,d=!1;for(;e<s;){for(c=o[e];e<s&&void 0!==(v=i[e],n=null===(r=v)||void 0===r?void 0:r.It);)c+=n+o[++e],d=!0;a.push(v),$.push(c),e++}if(e===s&&$.push(o[s]),d){const t=$.join("$$lit$$");void 0===(o=l.get(t))&&l.set(t,o=$),i=a}return t(o,...i)},s=r(litHtml.html),n=r(litHtml.svg);exports.html=s,exports.svg=n,exports.unsafeStatic=t=>({It:t}),exports.withStatic=r,Object.defineProperty(exports,"__esModule",{value:!0})}));
---
+++ typo3/sysext/core/Resources/Public/JavaScript/Contrib/lit/hydrate-support.js
@@ -0,0 +1,1 @@
+define(["exports","lit-element/hydrate-support"],(function(exports,hydrateSupport){"use strict";Object.keys(hydrateSupport).forEach((function(k){"default"!==k&&Object.defineProperty(exports,k,{enumerable:!0,get:function(){return hydrateSupport[k]}})})),Object.defineProperty(exports,"__esModule",{value:!0})}));
