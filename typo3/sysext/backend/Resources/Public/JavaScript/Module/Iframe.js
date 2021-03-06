4b6e2e46600a706d87bbd2cc9da4a876cc29ea30
Benjamin Franzke <bfr@qbus.de>
2021-03-06 15:41:09 +0100

    [POC][WIP][FEATURE] Add support for custom HTML component based backend modules
    As alternative to iframe based backend modules,
    HTML components can now be registered that are
    used to implement modules.
    
    A module consists of a JavaScript module that
    provides an implementation for a custom HTML tag.
    This tag is created by the new <typo3-backend-module-router>
    when the route is opened via ModuleMenu or via deep link.
    
    The router currently uses two parameters for it's routing.
    A named module attribute, and a endpoint.
    That means, currently it route's mostly by module name, no
    client side URL-routing (yet – that's up to research/discussion)
    
    State is managed via the traditional iframe (instead of history.pushState).
    That may looks very strange, but we cannot combine implicit iframe
    history state changes with history.pushState – iframes will take
    precedence. Therefore we have to stick with iframe-state handling.
    The advantage is: All "tranditional" modules will preserve the
    state handling as before.
    
    To keep this change rather small, we do not adapt
    all users of ContentContainer/inline javascript
    that acceses top.list_frame. We rather rely on iframe
    events handlers in order to synchronize the router.
    
    TODO:
     * Enhance API to allow modules to push and/or replace state
     * Link based routing (either full client side routing,
       or backend site routing, would then require a data-module
       tag for links)
     * Improve navigation container synchronization (when opening deeplinks)
       Maybe move into the router, or extend the router to
       route the navigation container as well.
     * RST
    
    Resolves: #?
    Releases: master
    Change-Id: I682e89649b597c8c74b6a0a8f198f6bcf5bbc347

---
+++ typo3/sysext/backend/Resources/Public/JavaScript/Module/Iframe.js
@@ -0,0 +1,36 @@
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+var __decorate=this&&this.__decorate||function(e,t,o,n){var l,r=arguments.length,a=r<3?t:null===n?n=Object.getOwnPropertyDescriptor(t,o):n;if("object"==typeof Reflect&&"function"==typeof Reflect.decorate)a=Reflect.decorate(e,t,o,n);else for(var d=e.length-1;d>=0;d--)(l=e[d])&&(a=(r<3?l(a):r>3?l(t,o,a):l(t,o))||a);return r>3&&a&&Object.defineProperty(t,o,a),a};define(["require","exports","lit-element","TYPO3/CMS/Core/lit-helper"],(function(e,t,o,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0}),t.IframeModuleElement=void 0;let l=class extends o.LitElement{constructor(){super(...arguments),this.endpoint=""}static get styles(){return o.css`
+      :host {
+        display: block;
+        height: 100%;
+      }
+      iframe {
+        display: block;
+        border: none;
+        height: 100%;
+        width: 1px;
+        min-width: 100%;
+        transform: translate3d(0,0,0);
+      }
+    `}createRenderRoot(){return this}render(){const e=this.endpoint;return console.log("rendering iframe",{src:e}),e?o.html`
+      <iframe
+        name="list_frame"
+        id="typo3-contentIframe"
+        class="scaffold-content-module-iframe t3js-scaffold-content-module-iframe"
+        src="${e}"
+        title="${n.lll("iframe.listFrame")}"
+        scrolling="no"
+        @load="${this._load}"
+      ></iframe>
+    `:o.html``}connectedCallback(){super.connectedCallback();const e=new CustomEvent("typo3-module-load",{bubbles:!0,composed:!0,detail:{url:this.endpoint,decorate:!1}});console.log("connectedCallback",e),this.dispatchEvent(e)}attributeChangedCallback(e,t,o){if(super.attributeChangedCallback(e,t,o),"endpoint"===e){const e=this.renderRoot.querySelector("iframe");e&&e.contentWindow.location.reload()}}_load(e){const t=e.target;let o=null,n=null;try{o=t.contentWindow.location.href;const e=t.contentDocument.body.querySelector(".module[data-module-name]");n=e&&e.getAttribute("data-module-name")||null,t.contentWindow.addEventListener("unload",e=>{console.log("real iframe unload",e),new Promise(e=>window.setTimeout(e,0)).then(()=>{if(null===t.contentWindow)return void console.log("real iframe window not found. we probably got removed.");const e=t.contentWindow.location.href,o=new CustomEvent("typo3-module-load",{bubbles:!0,composed:!0,detail:{url:e,decorate:!0}});this.dispatchEvent(o)})},{once:!0})}catch(e){console.log("iframe catch",e)}console.log("loaded iframe event",e,{url:o,module:n},t===e.target);const l=new CustomEvent("typo3-module-loaded",{bubbles:!0,composed:!0,detail:{url:o,module:n}});console.log("sending out an url change "+o),this.dispatchEvent(l)}};__decorate([o.property({type:String})],l.prototype,"endpoint",void 0),l=__decorate([o.customElement("typo3-iframe-module")],l),t.IframeModuleElement=l}));
