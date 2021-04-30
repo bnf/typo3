e500f60cacdbc8f212cfde2b812313d5c897d358
Benjamin Franzke <bfr@qbus.de>
2021-04-30 22:54:19 +0200

    module-docheader
    Change-Id: Ie2afaf23439174df7c30a4dcfac85d25618d2812

--- typo3/sysext/backend/Resources/Public/JavaScript/Element/Module.js
+++ typo3/sysext/backend/Resources/Public/JavaScript/Element/Module.js
@@ -10,7 +10,7 @@
  *
  * The TYPO3 project - inspiring people to share!
  */
-var __decorate=this&&this.__decorate||function(e,o,d,t){var r,i=arguments.length,l=i<3?o:null===t?t=Object.getOwnPropertyDescriptor(o,d):t;if("object"==typeof Reflect&&"function"==typeof Reflect.decorate)l=Reflect.decorate(e,o,d,t);else for(var a=e.length-1;a>=0;a--)(r=e[a])&&(l=(i<3?r(l):i>3?r(o,d,l):r(o,d))||l);return i>3&&l&&Object.defineProperty(o,d,l),l};define(["require","exports","lit","lit/decorators"],(function(e,o,d,t){"use strict";Object.defineProperty(o,"__esModule",{value:!0}),o.ModuleElement=void 0;let r=class extends d.LitElement{constructor(){super(...arguments),this.size="small"}render(){return d.html`
+var __decorate=this&&this.__decorate||function(e,o,t,d){var r,i=arguments.length,l=i<3?o:null===d?d=Object.getOwnPropertyDescriptor(o,t):d;if("object"==typeof Reflect&&"function"==typeof Reflect.decorate)l=Reflect.decorate(e,o,t,d);else for(var a=e.length-1;a>=0;a--)(r=e[a])&&(l=(i<3?r(l):i>3?r(o,t,l):r(o,t))||l);return i>3&&l&&Object.defineProperty(o,t,l),l};define(["require","exports","lit","lit/decorators"],(function(e,o,t,d){"use strict";Object.defineProperty(o,"__esModule",{value:!0}),o.ModuleElement=void 0;let r=class extends t.LitElement{constructor(){super(...arguments),this.size="small"}render(){return t.html`
       <div class="module">
         <div class="module-loading-indicator"></div>
 
@@ -31,20 +31,20 @@ var __decorate=this&&this.__decorate||function(e,o,d,t){var r,i=arguments.length
           <slot></slot>
         </div>
       </div>
-    `}};r.styles=d.css`
+    `}};r.styles=t.css`
     :host {
       display: block;
       height: 100%;
     }
     .module-docheader {
       position: sticky;
-      top: 20px;
+      top: 16px;
       width: 100%;
       min-height: 65px;
       z-index: 300;
       background-color: #eee;
       border-bottom: 1px solid #c3c3c3;
-      padding: 0 24px;
+      padding: 4px 24px 0;
       box-sizing: border-box;
       transition: margin-top .3s ease-in-out;
     }
@@ -54,27 +54,27 @@ var __decorate=this&&this.__decorate||function(e,o,d,t){var r,i=arguments.length
       flex-wrap: wrap;
       justify-content: space-between;
     }
-
-    .module-docheader-bar > * {
-      box-sizing: border-box;
-      min-height: 26px;
-      margin: 4px 0;
-      line-height: 26px;
+    .module-docheader-bar > *:empty {
+      display: none;
     }
     .module-docheader-bar-navigation {
       min-height: 26px;
     }
-    .module-docheader-bar > *:empty {
-      display: none;
+    .module-docheader-bar-navigation > ::slotted(*) {
+      margin-bottom: 4px;
     }
-
+    .module-docheader-bar-buttons > * {
+      box-sizing: border-box;
+      min-height: 26px;
+      margin-bottom: 4px;
+      line-height: 26px;
+    }
     .module-docheader-bar-buttons-column-left,
     .module-docheader-bar-buttons-column-right {
       display: flex;
       flex-direction: row;
     }
-
     .module-body {
-      padding: 24px 24px;
+      padding: 24px;
     }
-  `,__decorate([t.property({type:String})],r.prototype,"size",void 0),r=__decorate([t.customElement("typo3-backend-module")],r),o.ModuleElement=r}));
+  `,__decorate([d.property({type:String})],r.prototype,"size",void 0),r=__decorate([d.customElement("typo3-backend-module")],r),o.ModuleElement=r}));
