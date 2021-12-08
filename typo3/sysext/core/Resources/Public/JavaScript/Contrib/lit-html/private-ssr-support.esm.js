f4df5ecf86d7a58ced43e51de0cb6d79f7d879aa
Benjamin Franzke <bfr@qbus.de>
2021-12-08 22:12:24 +0100

    [TASK] Compile ESM modules
    Change-Id: I295234f6be0338e844f4f097f6db24d36887e97e

---
+++ typo3/sysext/core/Resources/Public/JavaScript/Contrib/lit-html/private-ssr-support.esm.js
@@ -0,0 +1,6 @@
+import{_$LH as R,noChange as T}from"./lit-html.esm.js";
+/**
+ * @license
+ * Copyright 2019 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */const r={boundAttributeSuffix:R.P,marker:R.V,markerMatch:R.L,HTML_RESULT:R.I,getTemplateHtml:R.N,overrideDirectiveResolve:(t,e)=>class extends t{_$AS(t,r){return e(this,r)}},setDirectiveClass(t,e){t._$litDirective$=e},getAttributePartCommittedValue:(t,r,i)=>{let a=T;return t.k=t=>a=t,t._$AI(r,t,i),a},connectedDisconnectable:t=>({...t,_$AU:!0}),resolveDirective:R.j,AttributePart:R.O,PropertyPart:R.W,BooleanAttributePart:R.F,EventPart:R.B,ElementPart:R.Z};export{r as _$LH};
