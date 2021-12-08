16370d05662d8727d6f683c8cdae3402c2f9e037
Benjamin Franzke <bfr@qbus.de>
2021-12-08 21:21:41 +0100

    [WIP][TASK] Use ES6 modules and imports
    Gruntfile: Build esm modules and transform contribs to ES6
    
    Also use allowSyntheticDefaultImports and disable esModuleInterop.
    allowSyntheticDefaultImports had been enabled implicitly by esModuleInterop.
    esModuleInterop is no longer needed, as we target ESmodules, but we need
    to enable allowSyntheticDefaultImports as the type declarations still
    imply AMD/CJS exports (which we fix in our build steps).
    
    yarn add --dev es-module-lexer
    
    Releases: main
    Resolves: #
    Change-Id: Ic219af6a169d566d6052aaece5ea1574cff75d55

---
+++ typo3/sysext/recycler/Configuration/JavaScriptModules.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'dependencies' => [
+        'backend',
+        'core',
+    ],
+    'backend' => [
+        'imports' => [
+            'TYPO3/CMS/Recycler/' => 'EXT:recycler/Resources/Public/JavaScript/',
+        ],
+    ],
+];
