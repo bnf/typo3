f11f12d22cd9547375481768a9b27e4496676f91
Benjamin Franzke <bfr@qbus.de>
2021-12-08 22:12:24 +0100

    [TASK] Compile ESM modules and delete old contribs
     #git rm typo3/sysext/core/Resources/Public/JavaScript/Contrib/jquery.autocomplete.js
    git rm typo3/sysext/core/Resources/Public/JavaScript/Contrib/bootstrap/bootstrap.js
    git rm typo3/sysext/core/Resources/Public/JavaScript/Contrib/broadcastchannel-polyfill.js
    
    find typo3/sysext/ -name '*.esm.js' | \
        grep -v autocomplete | grep -v bootstrap.esm | grep -v broadcastchannel.esm.js | \
        sed 's/\.esm\.js/.js/' | xargs ls -1 | xargs git rm
    
    git rm typo3/sysext/core/Resources/Public/JavaScript/Contrib/Sortable.min.js \
        typo3/sysext/core/Resources/Public/JavaScript/Contrib/cropper.min.js \
        typo3/sysext/core/Resources/Public/JavaScript/Contrib/imagesloaded.pkgd.min.js \
        typo3/sysext/core/Resources/Public/JavaScript/Contrib/jquery/jquery.js \
        typo3/sysext/core/Resources/Public/JavaScript/Contrib/jquery/jquery.min.js
    
    Change-Id: I84ee370fd511a781afeb5f322ca777ecf0e4c203

--- typo3/sysext/core/Resources/Public/JavaScript/Contrib/@lit/reactive-element/decorators/event-options.js
+++ typo3/sysext/core/Resources/Public/JavaScript/Contrib/@lit/reactive-element/decorators/event-options.js
@@ -1,6 +1,6 @@
-define(["exports","./base"],(function(exports,base){"use strict";
+import{decorateProperty}from"@lit/reactive-element/decorators/base.js";
 /**
-	 * @license
-	 * Copyright 2017 Google LLC
-	 * SPDX-License-Identifier: BSD-3-Clause
-	 */exports.eventOptions=function(e){return base.decorateProperty({finisher:(r,t)=>{Object.assign(r.prototype[t],e)}})},Object.defineProperty(exports,"__esModule",{value:!0})}));
+ * @license
+ * Copyright 2017 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */function e(e){return decorateProperty({finisher:(r,t)=>{Object.assign(r.prototype[t],e)}})}export{e as eventOptions};
