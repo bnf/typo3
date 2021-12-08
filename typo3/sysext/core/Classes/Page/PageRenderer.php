3c3496520d8aee17fceb237160345546a007db06
Benjamin Franzke <bfr@qbus.de>
2021-12-08 21:21:41 +0100

    [TASK] Gruntfile: Build esm modules and transform contribs to ES6
    Change-Id: Icee62a345cb7cc0159a37ec3f8f768eab89af7e8

--- typo3/sysext/core/Classes/Page/PageRenderer.php
+++ typo3/sysext/core/Classes/Page/PageRenderer.php
@@ -1408,7 +1408,13 @@ class PageRenderer implements SingletonInterface
 
         $publicPackageNames = ['core', 'frontend', 'backend'];
 
-        $aliases = [];
+        $aliases = [
+            'lit/index' => 'lit',
+            'lit-html/lit-html' => 'lit-html',
+            'lit-element/index' => 'lit-element',
+            '@lit/reactive-element/reactive-element' => '@lit/reactive-element',
+            'TYPO3/CMS/Dashboard/Contrib/web-animate' => 'web-animate',
+        ];
 
         $extensionVersions = [];
         foreach ($packages as $packageName => $package) {
@@ -1477,7 +1483,6 @@ class PageRenderer implements SingletonInterface
         $corePath = PathUtility::getAbsoluteWebPath($corePath);
         // first, load all paths for the namespaces, and configure contrib libs.
         $requireJsConfig['public']['paths'] = [
-            'jquery' => $corePath . 'jquery/jquery',
             'jquery-ui' => $corePath . 'jquery-ui',
             'nprogress' => $corePath . 'nprogress',
             'moment' => $corePath . 'moment',
@@ -1499,28 +1504,6 @@ class PageRenderer implements SingletonInterface
         ];
         $requireJsConfig['public']['shim'] = [
             'tablesort.dotsep' => ['deps' => ['tablesort']],
-        ];
-        $requireJsConfig['public']['packages'] = [
-            [
-                'name' => 'lit-html',
-                'location' => $corePath . 'lit-html',
-                'main' => 'lit-html',
-            ],
-            [
-                'name' => '@lit/reactive-element',
-                'location' => $corePath . '@lit/reactive-element',
-                'main' => 'reactive-element',
-            ],
-            [
-                'name' => 'lit-element',
-                'location' => $corePath . 'lit-element',
-                'main' => 'index',
-            ],
-            [
-                'name' => 'lit',
-                'location' => $corePath . 'lit',
-                'main' => 'index',
-            ],
         ];
         $requireJsConfig['public']['waitSeconds'] = 30;
         $requireJsConfig['public']['typo3BaseUrl'] = false;
