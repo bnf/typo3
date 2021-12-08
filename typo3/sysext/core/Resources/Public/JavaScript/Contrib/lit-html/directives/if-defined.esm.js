0c35ea5b58ec34e9cc9107a93194162216450993
Benjamin Franzke <bfr@qbus.de>
2021-12-08 22:12:24 +0100

    [TASK] Compile ESM modules
    Change-Id: I295234f6be0338e844f4f097f6db24d36887e97e

---
+++ typo3/sysext/core/Resources/Public/JavaScript/Contrib/lit-html/directives/if-defined.esm.js
@@ -0,0 +1,6 @@
+import{nothing as x}from"../lit-html.esm.js";
+/**
+ * @license
+ * Copyright 2018 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */const l=l=>null!=l?l:x;export{l as ifDefined};
