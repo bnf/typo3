82b8269349e2d01be8a08f71840b3e68bae5cbf4
Benjamin Franzke <bfr@qbus.de>
2021-12-08 22:12:24 +0100

    [TASK] Compile ESM modules and delete old contribs
     #git rm typo3/sysext/core/Resources/Public/JavaScript/Contrib/jquery.autocomplete.js
    git rm typo3/sysext/core/Resources/Public/JavaScript/Contrib/bootstrap/bootstrap.js
    git rm typo3/sysext/core/Resources/Public/JavaScript/Contrib/broadcastchannel-polyfill.js
    
    find typo3/sysext/ -name '*.esm.js' | \
        grep -v autocomplete | grep -v bootstrap.esm | grep -v broadcastchannel.esm.js | \
        sed 's/\.esm\.js/.js/' | xargs ls -1 | xargs git rm
    
    git rm typo3/sysext/core/Resources/Public/JavaScript/Contrib/Sortable.min.js \
        typo3/sysext/core/Resources/Public/JavaScript/Contrib/cropper.min.js \
        typo3/sysext/core/Resources/Public/JavaScript/Contrib/imagesloaded.pkgd.min.js \
        typo3/sysext/core/Resources/Public/JavaScript/Contrib/jquery/jquery.js \
        typo3/sysext/core/Resources/Public/JavaScript/Contrib/jquery/jquery.min.js
    
    Change-Id: I84ee370fd511a781afeb5f322ca777ecf0e4c203

--- typo3/sysext/core/Resources/Public/JavaScript/Contrib/lit-html/directives/guard.js
+++ typo3/sysext/core/Resources/Public/JavaScript/Contrib/lit-html/directives/guard.js
@@ -1,6 +1,6 @@
-define(["exports","../lit-html","../directive"],(function(exports,litHtml,directive){"use strict";
+import{directive,Directive}from"lit-html/directive.js";import{noChange}from"lit-html/lit-html.js";
 /**
-	 * @license
-	 * Copyright 2018 Google LLC
-	 * SPDX-License-Identifier: BSD-3-Clause
-	 */const e={},i=directive.directive(class extends directive.Directive{constructor(){super(...arguments),this.ot=e}render(r,t){return t()}update(t,[s,e]){if(Array.isArray(s)){if(Array.isArray(this.ot)&&this.ot.length===s.length&&s.every((r,t)=>r===this.ot[t]))return litHtml.noChange}else if(this.ot===s)return litHtml.noChange;return this.ot=Array.isArray(s)?Array.from(s):s,this.render(s,e)}});exports.guard=i,Object.defineProperty(exports,"__esModule",{value:!0})}));
+ * @license
+ * Copyright 2018 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */const e={},i=directive(class extends Directive{constructor(){super(...arguments),this.ot=e}render(r,t){return t()}update(t,[s,e]){if(Array.isArray(s)){if(Array.isArray(this.ot)&&this.ot.length===s.length&&s.every((r,t)=>r===this.ot[t]))return noChange}else if(this.ot===s)return noChange;return this.ot=Array.isArray(s)?Array.from(s):s,this.render(s,e)}});export{i as guard};
--- typo3/sysext/core/Resources/Public/JavaScript/Contrib/lit-html/directives/template-content.js
+++ typo3/sysext/core/Resources/Public/JavaScript/Contrib/lit-html/directives/template-content.js
@@ -1,6 +1,6 @@
-define(["exports","../lit-html","../directive"],(function(exports,litHtml,directive){"use strict";
+import{directive,Directive,PartType}from"lit-html/directive.js";import{noChange}from"lit-html/lit-html.js";
 /**
-	 * @license
-	 * Copyright 2020 Google LLC
-	 * SPDX-License-Identifier: BSD-3-Clause
-	 */const o=directive.directive(class extends directive.Directive{constructor(t){if(super(t),t.type!==directive.PartType.CHILD)throw Error("templateContent can only be used in child bindings")}render(r){return this.at===r?litHtml.noChange:(this.at=r,document.importNode(r.content,!0))}});exports.templateContent=o,Object.defineProperty(exports,"__esModule",{value:!0})}));
+ * @license
+ * Copyright 2020 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */const o=directive(class extends Directive{constructor(t){if(super(t),t.type!==PartType.CHILD)throw Error("templateContent can only be used in child bindings")}render(r){return this.at===r?noChange:(this.at=r,document.importNode(r.content,!0))}});export{o as templateContent};
