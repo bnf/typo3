62d2a3cf92101059542a70abd8c5f00219ed3724
Benjamin Franzke <bfr@qbus.de>
2021-12-08 21:21:41 +0100

    [TASK] Migrate backend JavaScript modules to ES6
    Use the JavaScript module and importmap Feature
    introduced in #96510 to migrate our JavaScript modules
    to ES6. Backwards compatibility for existing RequireJS
    imports is provided by a shim as introduced in #96510.
    
    How to (re)view this change
    ---------------------------
    
    Exclude all automatic TypeScript/JavaScript/lockfile changes with:
    
    git show --oneline -- . \
        ':(exclude)typo3/sysext/*.js' \
        ':(exclude)Build/Sources/TypeScript/*.ts' \
        ':(exclude)Build/yarn.lock'
    
    git show --oneline -- $(find Build/ \
        -name DragUploader.ts -o \
        -name FormEngine.ts -o \
        -name Helper.ts -o \
        -name CKEditorLoader.ts -o \
        -name CodeMirrorElement.ts)
    
    Gruntfile & tsconfig.json
    -------------------------
    
    Gruntfile is adapted to transform all our dependencies to ES6.
    
    Also tsconfig is adapted to use allowSyntheticDefaultImports and
    disable esModuleInterop. allowSyntheticDefaultImports had bee
    enabled implicitly by esModuleInterop. esModuleInterop is no longer
    needed, as we target ESmodules, but we need to enable
    allowSyntheticDefaultImports as the type declarations still
    imply AMD/CJS exports (which we fix in our build steps).
    
    JSUnit
    ------
    
    JSUnit configuration is moved from testing-framework into core for more
    flexibility when adaptions are required. The testing is revamped
    to use karma-rollup-preprocessor instead of karma-requirejs in
    order to support the newly compiled ES6 modules.
    
    TypeScript Exports and Imports
    ------------------------------
    
    All export and import definitions have been auto-converted
    from require-style imports to native ES6 import/exports.
    
    Executed commands:
    
      find Build/Sources/TypeScript/ -name '*.ts' -exec sed -i \
        -e 's/export =/export default/' \
        -e 's/ = require(\(.*\))/ from \1/g' \
        -e "s/^import 'tablesort';/import Tablesort from 'tablesort';/" \
        '{}' +
    
    Explicitly use RequireJS for CodeMirror and ext:form for now
    ------------------------------------------------------------
    
    CodeMirror can't be easily transformed to ES6 right now,
    therefore this patch explicitly configures to use the
    global RequireJS instance.
    
    The ext:form JavaScript is currently not written in TypeScript
    and will therefore not be automatically transformed from AND
    to ES6 when updating tsconfig.json to emit ES6 modules.
    Therefore requirejs is uses.
    
    This partially reverts #96326 which changed some require()
    calls to import() syntax too early.
    
    New npm dependencies:
    ---------------------
    
     # For module-post processing in Gruntfile
     # This is the same parser as used for the
     # frontend polyfill es-module-shims
    yarn add --dev es-module-lexer
    
     # For unit testing
    yarn remove karma-requirejs
    yarn add --dev karma-rollup-preprocessor rollup-plugin-glob-import
    
    Releases: main
    Resolves: #96511
    Related: #96510
    Related: #96323
    Change-Id: Id1a6d414c1e9b2e39227d0b6ce9e79333a372349

--- typo3/sysext/core/Resources/Public/JavaScript/Contrib/lit-html/directives/async-append.js
+++ typo3/sysext/core/Resources/Public/JavaScript/Contrib/lit-html/directives/async-append.js
@@ -1,6 +1,6 @@
-define(["exports","../directive-helpers","../directive","./async-replace"],(function(exports,directiveHelpers,directive,asyncReplace){"use strict";
+import{clearPart,insertPart,setChildPartValue}from"lit-html/directive-helpers.js";import{directive,PartType}from"lit-html/directive.js";import{AsyncReplaceDirective}from"lit-html/directives/async-replace.js";
 /**
-	 * @license
-	 * Copyright 2017 Google LLC
-	 * SPDX-License-Identifier: BSD-3-Clause
-	 */const c=directive.directive(class extends asyncReplace.AsyncReplaceDirective{constructor(r){if(super(r),r.type!==directive.PartType.CHILD)throw Error("asyncAppend can only be used in child expressions")}update(r,e){return this._$CX=r,super.update(r,e)}commitValue(r,e){0===e&&directiveHelpers.clearPart(this._$CX);const s=directiveHelpers.insertPart(this._$CX);directiveHelpers.setChildPartValue(s,r)}});exports.asyncAppend=c,Object.defineProperty(exports,"__esModule",{value:!0})}));
+ * @license
+ * Copyright 2017 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */const c=directive(class extends AsyncReplaceDirective{constructor(r){if(super(r),r.type!==PartType.CHILD)throw Error("asyncAppend can only be used in child expressions")}update(r,e){return this._$CX=r,super.update(r,e)}commitValue(r,e){0===e&&clearPart(this._$CX);const s=insertPart(this._$CX);setChildPartValue(s,r)}});export{c as asyncAppend};
