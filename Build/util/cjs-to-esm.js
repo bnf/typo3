c7962f7f12556eafbb878c78fa54e4172f85616b
Benjamin Franzke <bfr@qbus.de>
2021-12-08 21:21:41 +0100

    [TASK] Gruntfile: Build esm modules and transform contribs to ES6
    Change-Id: Icee62a345cb7cc0159a37ec3f8f768eab89af7e8

---
+++ Build/util/cjs-to-esm.js
@@ -0,0 +1,47 @@
+const provideImports = (imports) => {
+  const src = new Array();
+  imports.forEach(module => {
+    const variableName = '__import_' + module.replace('/', '_').replace('@','').replace('-', '_')
+    src.push('import ' + variableName + ' from "' + module + '"')
+  })
+
+  src.push('var require = function(name) {');
+  src.push('  switch (name) {');
+  imports.forEach(module => {
+    const variableName = '__import_' + module.replace('/', '_').replace('@','').replace('-', '_')
+    src.push('  case "' + module + '":');
+    src.push('    return ' + variableName)
+  })
+
+  src.push('  }');
+  src.push('  throw new Error("module " + name + " missing")')
+  src.push('}');
+
+  return src.join('\n');
+}
+
+const cjsToEsm = (source, imports, prefix) => {
+  imports = imports || [];
+  prefix = prefix || ''
+  const code = [
+    'export default (new function () {',
+    '  const module = { exports: {} };',
+    '  const exports = module.exports;',
+    '  const define = null;',
+    source,
+    '  this.module = module',
+    '}).module.exports'
+  ];
+
+  if (imports.length > 0) {
+    code.unshift(provideImports(imports));
+  }
+
+  if (prefix) {
+    code.unshift(prefix);
+  }
+
+  return code.join('\n');
+}
+
+exports.cjsToEsm = cjsToEsm;
