f09fa986011eac821d9ab866bfa0db8705a73dd0
Benjamin Franzke <bfr@qbus.de>
2021-12-08 21:21:37 +0100

    [TASK] manual modifications
    Change-Id: I76906a1d4dbc34b5a0b7f21c44a5bdbc98d62aad

--- Build/Sources/TypeScript/backend/Resources/Public/TypeScript/Element/ImmediateActionElement.ts
+++ Build/Sources/TypeScript/backend/Resources/Public/TypeScript/Element/ImmediateActionElement.ts
@@ -30,13 +30,13 @@ export class ImmediateActionElement extends HTMLElement {
     switch (action) {
       case 'TYPO3.ModuleMenu.App.refreshMenu':
         const moduleMenuApp = await import('TYPO3/CMS/Backend/ModuleMenu');
-        return moduleMenuApp.App.refreshMenu.bind(moduleMenuApp.App);
+        return moduleMenuApp.default.App.refreshMenu.bind(moduleMenuApp.default.App);
       case 'TYPO3.Backend.Topbar.refresh':
         const viewportObject = await import('TYPO3/CMS/Backend/Viewport');
-        return viewportObject.Topbar.refresh.bind(viewportObject.Topbar);
+        return viewportObject.default.Topbar.refresh.bind(viewportObject.default.Topbar);
       case 'TYPO3.WindowManager.localOpen':
         const windowManager = await import('TYPO3/CMS/Backend/WindowManager');
-        return windowManager.localOpen.bind(windowManager);
+        return windowManager.default.localOpen.bind(windowManager.default);
       case 'TYPO3.Backend.Storage.ModuleStateStorage.update':
         return (await import('TYPO3/CMS/Backend/Storage/ModuleStateStorage')).ModuleStateStorage.update;
       case 'TYPO3.Backend.Storage.ModuleStateStorage.updateWithCurrentMount':
