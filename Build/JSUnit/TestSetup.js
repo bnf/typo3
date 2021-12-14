d05d13a151f04482b8c5c7686d97e003d8be04e5
Benjamin Franzke <bfr@qbus.de>
2021-12-14 21:30:36 +0100

    [TASK] Adapt JSUnit tests (karma) for ES6 sources
    JSUnit configuration is moved
    from testing-framework into core for more
    flexibility when adaptions are required.
    
    yarn remove karma-requirejs
    yarn add --dev karma-rollup-preprocessor
    
    Releases: main
    Resolves: #
    Related: #
    Change-Id: Ie8b1a11525e90b410e6cb39453111e09fb650e12

---
+++ Build/JSUnit/TestSetup.js
@@ -0,0 +1,13 @@
+import './JQueryGlobal.js'
+
+// @todo: maybe use https://www.npmjs.com/package/rollup-plugin-glob-import
+import '../../typo3/sysext/core/Tests/JavaScript/Ajax/AjaxRequestTest.esm.js'
+import '../../typo3/sysext/core/Tests/JavaScript/Ajax/InputTransformerTest.esm.js'
+import '../../typo3/sysext/core/Tests/JavaScript/SecurityUtilityTest.esm.js'
+
+import '../../typo3/sysext/backend/Tests/JavaScript/BackendExceptionTest.esm.js'
+import '../../typo3/sysext/backend/Tests/JavaScript/Element/ImmediateActionElementTest.esm.js'
+import '../../typo3/sysext/backend/Tests/JavaScript/GridEditorTest.esm.js'
+import '../../typo3/sysext/backend/Tests/JavaScript/Hashing/Md5Test.esm.js'
+import '../../typo3/sysext/backend/Tests/JavaScript/NotificationTest.esm.js'
+
