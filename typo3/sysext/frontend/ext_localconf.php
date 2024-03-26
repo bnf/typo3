409ce53c64c9811dd3415873a70d72ad66edc03a
Benjamin Franzke <ben@bnf.dev>
2024-03-26 14:14:14 +0100

    [TASK] Move EXT:frontend default TypoScript to Configuration folder
    In preparation for more TypoScript in EXT:frontend, the
    inline TypoScript is not moved from ext_localconf.php
    to Configuration/TypoScript.
    
    AbstractInclude is adapted to ensure that the constants include
    survives a `unserialize(serialized(…))` chain correctly.
    
    Resolves: #103488
    Related: #103485
    Releases: main
    Change-Id: I5f73465db3347161a2896b90fee73ebeff0332c2

--- typo3/sysext/frontend/ext_localconf.php
+++ typo3/sysext/frontend/ext_localconf.php
@@ -10,34 +10,11 @@ defined('TYPO3') or die();
 // Register eID provider for showpic
 $GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include']['tx_cms_showpic'] = ShowImageController::class . '::processRequest';
 
+ExtensionManagementUtility::addTypoScriptConstants(
+    "@import 'EXT:frontend/Configuration/TypoScript/constants.typoscript'"
+);
 ExtensionManagementUtility::addTypoScriptSetup(
-    '
-# Content selection
-styles.content.get = CONTENT
-styles.content.get {
-    table = tt_content
-    select {
-        orderBy = sorting
-        where = {#colPos}=0
-    }
-}
-
-
-# Content element rendering
-tt_content = CASE
-tt_content {
-    key {
-        field = CType
-    }
-    default = TEXT
-    default {
-        field = CType
-        htmlSpecialChars = 1
-        wrap = <p style="background-color: yellow; padding: 0.5em 1em;"><strong>ERROR:</strong> Content Element with uid "{field:uid}" and type "|" has no rendering definition!</p>
-        wrap.insertData = 1
-    }
-}
-    '
+    "@import 'EXT:frontend/Configuration/TypoScript/setup.typoscript'"
 );
 
 // Register search key shortcuts
