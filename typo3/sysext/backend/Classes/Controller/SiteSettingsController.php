0f62b785d300d1343545bba69a47e61f2ecb769a
Benjamin Franzke <ben@bnf.dev>
2025-07-21 12:04:29 +0200

    [TASK] Include all importmap configuration in authorized contexts
    With modules working on the top frame (e.g. in modals)
    more and more importmap configurations (which are not runtime
    alterable) need to be loaded in the main frame anyway, therefore
    the tag-approach does no longer scale well.
    
    To avoid having to preemptively import all kind of dynamically
    loaded JavaScripts, all configured importmaps are now always
    loaded in authorized contexts.
    
    Releases: main
    Resolves: #107121
    Related: #107038
    Change-Id: I0a8dfb8e361852256b8624a98b389cc19b9e2d0c

--- typo3/sysext/backend/Classes/Controller/SiteSettingsController.php
+++ typo3/sysext/backend/Classes/Controller/SiteSettingsController.php
@@ -157,11 +157,6 @@ readonly class SiteSettingsController
         $this->addDocHeaderSiteConfigurationButton($view, $site);
         $this->pageRenderer->addInlineLanguageLabelFile('EXT:backend/Resources/Private/Language/locallang_copytoclipboard.xlf');
         $this->pageRenderer->addInlineLanguageLabelFile('EXT:backend/Resources/Private/Language/locallang_sitesettings.xlf');
-        foreach ($this->settingsTypeRegistry->getIdentifiers() as $type) {
-            $this->pageRenderer->getJavaScriptRenderer()->includeImportsFor(
-                $this->settingsTypeRegistry->get($type)->getJavaScriptModule()
-            );
-        }
 
         $view->assign('site', $site);
         $view->assign('siteTitle', $this->getSiteTitle($site));
