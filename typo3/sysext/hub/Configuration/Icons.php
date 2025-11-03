eb9003fa31dd7ccffb2ae1d5f94f72e48e1443b4
Benjamin Franzke <ben@bnf.dev>
2025-11-03 18:27:15 +0100

    [WIP][FEATURE] API management Hub
    Releases: main
    Resolves: #
    Change-Id: I4c2bf613b592221f458cf88c74c8fe4f6ee4bd9d

---
+++ typo3/sysext/hub/Configuration/Icons.php
@@ -0,0 +1,10 @@
+<?php
+
+use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
+
+return [
+    'module-hub' => [
+        'provider' => SvgIconProvider::class,
+        'source' => 'EXT:hub/Resources/Public/Icons/Extension.svg',
+    ],
+];
