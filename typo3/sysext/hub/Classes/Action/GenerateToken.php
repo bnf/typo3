fba6d0e8faac9b6ef366b8439c038141364e1006
Benjamin Franzke <ben@bnf.dev>
2025-11-03 18:27:15 +0100

    [WIP][FEATURE] API management Hub
    Releases: main
    Resolves: #
    Change-Id: I4c2bf613b592221f458cf88c74c8fe4f6ee4bd9d

---
+++ typo3/sysext/hub/Classes/Action/GenerateToken.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace TYPO3\CMS\Hub\Action;
+
+use TYPO3\CMS\Core\Action\ActionType;
+use TYPO3\CMS\Core\Attribute\AsAction;
+use TYPO3\CMS\Core\Security\JwtTrait;
+use TYPO3\CMS\Hub\Http\Middleware\AppResolver;
+use TYPO3\CMS\Hub\Repository\AppRepository;
+
+final readonly class GenerateToken
+{
+    use JwtTrait;
+
+    public function __construct(
+        private AppRepository $appRepository,
+    ) {}
+
+    /**
+     * @return array{token: string}
+     */
+    #[AsAction(
+        name: 'token/generate',
+        type: ActionType::create,
+    )]
+    public function perform(
+        string $appIdentifier,
+        string $secret,
+    ): array {
+        $app = $this->appRepository->getAppRecordByIdentifier($appIdentifier);
+
+        $token = self::encodeHashSignedJwt(
+            [
+                'identifier' => $app->getIdentifier(),
+                'secret' => $secret,
+                'time' => (new \DateTimeImmutable())->format(\DateTimeImmutable::RFC3339),
+                'mode' => 'static',
+            ],
+            self::createSigningKeyFromEncryptionKey(AppResolver::class)
+        );
+        return [
+            'token' => $token,
+        ];
+    }
+}
