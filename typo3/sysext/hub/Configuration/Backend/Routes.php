94bf5413b7d3174b7e0ff8c1864d1ac20b0f18e9
Benjamin Franzke <ben@bnf.dev>
2025-11-03 18:27:15 +0100

    [WIP][FEATURE] API management Hub
    Releases: main
    Resolves: #
    Change-Id: I4c2bf613b592221f458cf88c74c8fe4f6ee4bd9d

---
+++ typo3/sysext/hub/Configuration/Backend/Routes.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Definitions for routes provided by EXT:hub
+ */
+return [
+    'api' => [
+        //'path' => '/api/{handler?}',
+        'path' => '/api',
+        /*
+        'requirements' => [
+            'handler' => '.+',
+        ],
+         */
+        'access' => 'public',
+        'methods' => ['GET'],
+        'target' => \TYPO3\CMS\Hub\Http\AppHandler::class . '::dummyAction',
+    ],
+];
