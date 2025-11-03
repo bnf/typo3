55b32d640464332433dc42dfd0fcc1b00d18d9c8
Benjamin Franzke <ben@bnf.dev>
2025-11-03 18:27:15 +0100

    [WIP][FEATURE] API management Hub
    Releases: main
    Resolves: #
    Change-Id: I4c2bf613b592221f458cf88c74c8fe4f6ee4bd9d

---
+++ typo3/sysext/hub/ext_emconf.php
@@ -0,0 +1,19 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'TYPO3 CMS API Hub',
+    'description' => 'API for TYPO3',
+    'category' => 'module',
+    'author' => 'TYPO3 Core Team',
+    'author_email' => 'typo3cms@typo3.org',
+    'state' => 'stable',
+    'author_company' => '',
+    'version' => '14.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '14.0.0',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
