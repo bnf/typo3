c2cf1efd13428966c3d483522fab692b25c5d308
Benjamin Franzke <ben@bnf.dev>
2025-11-12 18:09:25 +0100

    [WIP][FEATURE] Provide OpenAPI spec w/ Swagger UI for Actions API
    Commands executed:
      npm install --save swagger-ui-dist
      npm install --save-dev @types/swagger-ui-dist
      npm run build
      # already done in previous commits:
      #composer req devizzent/cebe-php-openapi
    
    Releases: main
    Resolves: #
    Change-Id: I54da4f59cf07af56fb1b766fbef7ff9e89ef51d0

---
+++ Build/Sources/JavaScript/swagger-ui.js
@@ -0,0 +1,4 @@
+import SwaggerUIBundle from 'swagger-ui-dist/swagger-ui-bundle.js';
+//const SwaggerUIStandalonePreset = require('swagger-ui-dist/swagger-ui-standalone-preset.js');
+
+export { SwaggerUIBundle /*, SwaggerUIStandalonePreset*/ }
---
+++ typo3/sysext/backend/Classes/Controller/OpenApiController.php
@@ -0,0 +1,166 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace TYPO3\CMS\Backend\Controller;
+
+use cebe\openapi\Reader;
+use cebe\openapi\spec\Components;
+use cebe\openapi\spec\Info;
+use cebe\openapi\spec\OpenApi;
+use cebe\openapi\spec\Operation;
+use cebe\openapi\spec\PathItem;
+use cebe\openapi\spec\Paths;
+use cebe\openapi\spec\Response;
+use cebe\openapi\spec\Responses;
+use cebe\openapi\spec\Schema;
+use cebe\openapi\spec\Server;
+use cebe\openapi\spec\Tag;
+use TYPO3\CMS\Core\Action\ActionContext;
+use TYPO3\CMS\Core\Action\ActionRegistry;
+use TYPO3\CMS\Core\Attribute\AsAction;
+use TYPO3\CMS\Core\Information\Typo3Version;
+use TYPO3\CMS\Core\Routing\BackendEntryPointResolver;
+use TYPO3\CMS\Core\Schema\TcaSchemaFactory;
+
+/**
+ * @todo rename into Action\OpenapiSchema
+ */
+class OpenApiController
+{
+    public function __construct(
+        private readonly BackendEntryPointResolver $backendEntryPointResolver,
+        private readonly TcaSchemaFactory $tcaSchemaFactory,
+        private readonly ActionRegistry $actionRegistry,
+    ) {}
+
+    /**
+     * @return mixed
+     */
+    #[AsAction(
+        name: 'schema',
+        method: 'GET',
+    )]
+    public function getSchema(
+        ActionContext $context
+    ): mixed {
+        $lang = $context->languageService;
+        $request = $context->request;
+        $paths = [
+            /*
+            '/test' => new PathItem([
+                'description' => 'something',
+                'get' => new Operation([
+                    'summary' => 'foo',
+                    'description' => 'bar',
+                    'responses' => new Responses([
+                        '200' => new Response([
+                            'description' => 'baz',
+                            'content' => [
+                                'application/json' => [
+                                    'schema' => [
+                                        'type' => 'string'
+                                    ],
+                                ],
+                            ],
+                        ]),
+                    ]),
+                ]),
+            ])
+            */
+        ];
+
+        $server = (string)$this->backendEntryPointResolver->getUriFromRequest($context->request);
+
+        foreach ($this->actionRegistry->getItems() as $action) {
+            $operations = str_replace('"#/$defs/', '"#/components/schemas/', $action['operations']);
+            $pathItem = Reader::readFromJson($operations, PathItem::class);
+            $pathName = '/' . $action['route'];
+            if (isset($paths[$pathName])) {
+                $pathItem = new PathItem([
+                    ...$paths[$pathName]->getOperations(),
+                    ...$pathItem->getOperations(),
+                ]);
+            }
+            $paths[$pathName] = $pathItem;
+        }
+
+        $tags = [];
+        $tags[] = new Tag([
+            'name' => 'api',
+            'description' => 'TYPO3 command API',
+        ]);
+        $tags[] = new Tag([
+            'name' => 'record',
+            'description' => 'Fetch and mutate TYPO3 records (TcaSchema)',
+        ]);
+
+        $schemas = [];
+        foreach ($this->actionRegistry->listSchemas() as $schema) {
+            $schemas[$schema] = $this->actionRegistry->getSchema($schema, 'components/schemas');
+        }
+        foreach ($this->tcaSchemaFactory->all() as $schema) {
+            $name = 'TYPO3.CMS.Core.Domain.RecordInterface_' . $schema->getName() . '_';
+            $schemas[$name] = new Schema([
+                'type' => 'object',
+                'title' => $schema->getTitle($lang->sL(...)),
+                'description' => $name,
+                'properties' => array_map(
+                    static fn($field) => new Schema([
+                        'description' => $lang->sL($field->getLabel()) ?: $field->getLabel(),
+                        'default' => $field->hasDefaultValue() ? $field->getDefaultValue() : null,
+                        'type' => match ($field->getType()) {
+                            'json' => 'object',
+                            // @todo check if $field count(config.items) = 1, otherwise it is a bitset (better be expressed as a map in API)
+                            'check' => 'boolean',
+                            // @todo
+                            'file' => 'integer',
+                            'number' => 'integer',
+                            default => 'string',
+                        },
+                        // @todo add `additionalProperties: true` for type json
+                        // @todo support defining schematas in type=json and reference them here
+                    ]),
+                    iterator_to_array($schema->getFields()),
+                ),
+            ]);
+        }
+
+        $openapi = new OpenApi([
+            'openapi' => '3.1.2',
+            'info' => new Info([
+                'title' => 'TYPO3 API',
+                'version' => (new Typo3Version())->getVersion(),
+            ]),
+            'paths' => new Paths($paths),
+            'tags' => $tags,
+            'components' => new Components([
+                'schemas' => $schemas,
+            ]),
+            'servers' => [
+                new Server([
+                    'url' => rtrim($server, '/') . '/api',
+                ]),
+            ],
+        ]);
+
+        if (!$openapi->validate()) {
+            throw new \RuntimeException('OpenAPI Schema is invalid: ' . json_encode($openapi->getErrors()), 1768062644);
+        }
+
+        return $openapi->getSerializableData();
+    }
+}
