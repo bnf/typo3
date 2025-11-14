8b0e2938a55a1ed916083efcf2bf6d3c2055f122
Benjamin Franzke <ben@bnf.dev>
2025-11-14 16:42:30 +0100

    [WIP] oauth
    composer req league/oauth2-server
    
    Change-Id: Ic029d21453f52fea5f74540fac526bb91c8170f0

---
+++ typo3/sysext/hub/Classes/Repository/OAuth/OAuthScopeRepository.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace TYPO3\CMS\Hub\Repository\OAuth;
+
+use App\Model\OAuth\OAuthScopeModel;
+use App\Repository\Repository;
+use League\OAuth2\Server\Entities\ClientEntityInterface;
+use League\OAuth2\Server\Entities\ScopeEntityInterface;
+use League\OAuth2\Server\Repositories\ScopeRepositoryInterface;
+
+/**
+ * Class OAuthScopeRepository
+ *
+ * @package App\Repository\Client
+ *
+ * @author Jerfeson Guerreiro <jerfeson_guerreiro@hotmail.com>
+ *
+ * @since 1.0.0
+ *
+ * @version 1.0.0
+ *
+ */
+class OAuthScopeRepository extends Repository implements ScopeRepositoryInterface
+{
+    protected $modelClass = OAuthScopeModel::class;
+
+    /**
+     * Return information about a scope.
+     *
+     * @param string $identifier The scope identifier
+     *
+     * @return void
+     */
+    public function getScopeEntityByIdentifier($identifier)
+    {
+        // TODO: Implement getScopeEntityByIdentifier() method.
+    }
+
+    /**
+     * Given a client, grant type and optional user identifier validate the set of scopes requested are valid and optionally
+     * append additional scopes or remove requested scopes.
+     *
+     * @param ScopeEntityInterface[] $scopes
+     * @param string $grantType
+     * @param ClientEntityInterface $clientEntity
+     * @param string|null $userIdentifier
+     *
+     * @return ScopeEntityInterface[]
+     */
+    public function finalizeScopes(array $scopes, $grantType, ClientEntityInterface $clientEntity, $userIdentifier = null)
+    {
+        return [];
+    }
+}
