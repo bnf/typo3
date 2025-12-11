bd98b4e482623b49de45352429905d0caa35a26a
Benjamin Franzke <ben@bnf.dev>
2025-12-11 11:57:04 +0100

    [WIP][TASK] Add record serializer
    Releases: main
    Resolves: #
    Change-Id: Idb35eb28830066d73bf9667ae6744aaff8bc80b6

---
+++ typo3/sysext/core/Classes/Domain/RecordSerializer.php
@@ -0,0 +1,91 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace TYPO3\CMS\Core\Domain;
+
+use TYPO3\CMS\Core\Collection\LazyRecordCollection;
+use TYPO3\CMS\Core\Domain\Record;
+use TYPO3\CMS\Core\Domain\Record\LanguageInfo;
+use TYPO3\CMS\Core\Domain\Record\VersionInfo;
+use TYPO3\CMS\Core\Resource\Collection\LazyFileReferenceCollection;
+use TYPO3\CMS\Core\Resource\Collection\LazyFolderCollection;
+use TYPO3\CMS\Core\Resource\FileInterface;
+
+/**
+ * @internal not part of TYPO3 Core API yet.
+ */
+final readonly class RecordSerializer
+{
+    public function serialize(
+        RecordInterface $record
+    ): ?array {
+        $result = $this->doSerialize($record);
+        if (is_array($result) || $result === null) {
+            return $result;
+        }
+        throw new \LogicException('RecordSerializer::doSerialize returned a non-array for a Record', 1765453540);
+    }
+
+    private function doSerialize(
+        RecordInterface|
+        LazyFileReferenceCollection|
+        LazyFolderCollection|
+        LazyRecordCollection|
+        FileInterface|
+        \JsonSerializable|
+        \DateTimeInterface|
+        LanguageInfo|
+        VersionInfo|
+        FlexFormFieldValues|
+        array|string|float|int|bool|null $value
+    ): array|string|float|int|bool|null {
+        return match (true) {
+            $value instanceof RecordInterface => array_map(
+                fn(mixed $value): mixed => $this->doSerialize($value),
+                $value instanceof Record ? $value->toArray(true) : $value->toArray(),
+            ),
+            $value instanceof LazyRecordCollection => array_map(
+                fn(mixed $value): mixed => $this->doSerialize($value),
+                iterator_to_array($value),
+            ),
+            $value instanceof LazyFileReferenceCollection => array_map(
+                fn(mixed $value): mixed => $this->doSerialize($value),
+                iterator_to_array($value),
+            ),
+            $value instanceof LazyFolderCollection => array_map(
+                fn(mixed $value): mixed => $this->doSerialize($value),
+                iterator_to_array($value),
+            ),
+            $value instanceof FileInterface => $value->toArray(),
+            $value instanceof LanguageInfo => [
+                'languageId' => $value->getLanguageId(),
+                'translationParent' => $value->getTranslationParent(),
+                'translationSource' => $value->getTranslationSource(),
+            ],
+            $value instanceof VersionInfo => [ /* @todo  */ ],
+            $value instanceof FlexFormFieldValues => [ /* @todo */ ],
+            $value instanceof \JsonSerializable => $value->jsonSerialize(),
+            // @todo
+            $value instanceof \DateTimeInterface => $value->format(\DateTimeInterface::ATOM),
+            is_array($value) => array_map(
+                fn(mixed $value): mixed => $this->doSerialize($value),
+                $value,
+            ),
+            default => $value,
+        };
+    }
+}
