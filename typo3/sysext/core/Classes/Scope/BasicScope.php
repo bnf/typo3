3f6f20dfc7711deb46af9abff5e6f867fe429b1f
Benjamin Franzke <ben@bnf.dev>
2025-12-15 09:55:35 +0100

    [WIP][FEATURE] Introduce Action API
    The Action API acts as a multi purpose action
    RPC mechanism, that will support
    OpenAPI 3.1 and JsonSchema in order to provide
    interaction points for remote systems interacting
    with TYPO3.
    
      composer require \
        devizzent/cebe-php-openapi:^1.1 \
        justinrainbow/json-schema:^6.6
      composer require -d typo3/sysext/core --no-update \
        devizzent/cebe-php-openapi:^1.1 \
        justinrainbow/json-schema:^6.6
    
    Releases: main
    Resolves: #
    Change-Id: I242eedc16bb7ca1e5c83adeaa0526a9e68f275e2

---
+++ typo3/sysext/core/Classes/Scope/BasicScope.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace TYPO3\CMS\Core\Scope;
+
+use Symfony\Component\DependencyInjection\Attribute\AsTaggedItem;
+
+#[AsTaggedItem(index: 'basic', priority: 100)]
+final readonly class BasicScope implements ScopeInterface
+{
+    public function getIdentifier(): string
+    {
+        return 'basic';
+    }
+
+    public function getName(): string
+    {
+        return 'Basic';
+    }
+
+    public function getDescription(): string
+    {
+        return 'Basic details about you';
+    }
+
+    public function getIcon(): string
+    {
+        return 'actions-user';
+    }
+
+    public function allowedForUser(ScopeUser $user): bool
+    {
+        return true;
+    }
+}
---
+++ typo3/sysext/core/Classes/Scope/EmailScope.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace TYPO3\CMS\Core\Scope;
+
+use Symfony\Component\DependencyInjection\Attribute\AsTaggedItem;
+
+#[AsTaggedItem(index: 'email', priority: 99)]
+final readonly class EmailScope implements ScopeInterface
+{
+    public function getIdentifier(): string
+    {
+        return 'email';
+    }
+
+    public function getName(): string
+    {
+        return 'E-Mail Address';
+    }
+
+    public function getDescription(): string
+    {
+        return 'Your email address';
+    }
+
+    public function getIcon(): string
+    {
+        return 'actions-envelope';
+    }
+
+    public function allowedForUser(ScopeUser $user): bool
+    {
+        return true;
+    }
+}
