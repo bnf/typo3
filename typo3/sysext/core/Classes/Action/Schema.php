a6f32d8af617837915040d9762a50941a7056020
Benjamin Franzke <ben@bnf.dev>
2025-12-15 09:55:35 +0100

    [WIP][FEATURE] Introduce Action API
    The Action API acts as multi purpose action
    RPC mechanism, that will support
    OpenAPI 3.1 and JsonSchema in order to provide
    interaction points for remote systems interacting
    with TYPO3.
    
      composer require \
        devizzent/cebe-php-openapi:^1.1 \
        justinrainbow/json-schema:^6.6
      composer require -d typo3/sysext/core --no-update \
        devizzent/cebe-php-openapi:^1.1 \
        justinrainbow/json-schema:^6.6
    
    Releases: main
    Resolves: #
    Change-Id: I242eedc16bb7ca1e5c83adeaa0526a9e68f275e2

---
+++ typo3/sysext/core/Classes/Action/Schema.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace TYPO3\CMS\Core\Action;
+
+use cebe\openapi\spec\Schema as BaseSchema;
+use cebe\openapi\spec\Type;
+
+/**
+ * @internal
+ */
+final class Schema extends BaseSchema
+{
+    protected function attributes(): array
+    {
+        return [
+            ...parent::attributes(),
+            '$defs' => [Type::STRING, BaseSchema::class],
+        ];
+    }
+
+    protected function attributeDefaults(): array
+    {
+        return [
+            ...parent::attributeDefaults(),
+            '$defs' => null,
+        ];
+    }
+}
