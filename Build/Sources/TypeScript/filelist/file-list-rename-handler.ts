77885cfe22242746ee0f26618e05baf604fb99ec
Benjamin Franzke <ben@bnf.dev>
2025-12-20 10:05:50 +0100

    [TASK] RenameResource action
    Change-Id: I1c294383d6e9a835e3bd2b530a6687e55cc04fda

--- Build/Sources/TypeScript/filelist/file-list-rename-handler.ts
+++ Build/Sources/TypeScript/filelist/file-list-rename-handler.ts
@@ -27,6 +27,8 @@ interface Message {
   message: string;
 }
 
+const asJson = { headers: { 'Content-Type': 'application/json' } };
+
 class FileListRenameHandler {
   constructor() {
 
@@ -66,11 +68,10 @@ class FileListRenameHandler {
             const resourceName = submittedData.name.toString();
             if (resource.name !== resourceName) {
               const request = new AjaxRequest(TYPO3.settings.ajaxUrls.resource_rename);
-              request.post({
-                identifier: resource.identifier,
-                resourceName: resourceName,
-              }).then(async (success: AjaxResponse): Promise<void> => {
-
+              request.post(
+                { resourceIdentifier: resource.identifier, resourceName: resourceName },
+                asJson
+              ).then(async (success: AjaxResponse): Promise<void> => {
                 const data = await success.resolve();
                 if (data.status.length > 0) {
                   data.status.forEach((message: Message): void => {
