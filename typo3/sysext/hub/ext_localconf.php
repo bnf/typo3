52fc31423910993916120f02a0431e38396532da
Benjamin Franzke <ben@bnf.dev>
2025-12-20 19:36:58 +0100

    fieldmap element
    This reverts commit 15e59d63969d0fd887c6cdf28694f3146d8a0459.

--- typo3/sysext/hub/ext_localconf.php
+++ typo3/sysext/hub/ext_localconf.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use TYPO3\CMS\Hub\Form\FieldControl\TokenGenerator;
+use TYPO3\CMS\Hub\Form\Element\FieldMapElement;
 
 defined('TYPO3') or die();
 
@@ -10,4 +11,10 @@ $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1766577484] = [
     'nodeName' => 'tokenGenerator',
     'priority' => 40,
     'class' => TokenGenerator::class,
+];
+
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1660911089] = [
+    'nodeName' => 'fieldMap',
+    'priority' => 40,
+    'class' => FieldMapElement::class,
 ];
