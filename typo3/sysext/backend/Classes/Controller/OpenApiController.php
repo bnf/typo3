be5ccc68ebd64c4c4ccfbbe60c37d7f769d7b4f7
Benjamin Franzke <ben@bnf.dev>
2025-12-21 07:02:07 +0100

    Announce webhooks in openapi

--- typo3/sysext/backend/Classes/Controller/OpenApiController.php
+++ typo3/sysext/backend/Classes/Controller/OpenApiController.php
@@ -47,6 +47,7 @@ use TYPO3\CMS\Core\Information\Typo3Version;
 use TYPO3\CMS\Core\Routing\BackendEntryPointResolver;
 use TYPO3\CMS\Core\Schema\TcaSchemaFactory;
 use TYPO3\CMS\Core\Scope\ScopeRegistry;
+use TYPO3\CMS\Webhooks\WebhookTypesRegistry;
 
 /**
  * @todo rename into Action\OpenapiSchema
@@ -55,6 +56,7 @@ final readonly class OpenApiController
 {
     public function __construct(
         private BackendEntryPointResolver $backendEntryPointResolver,
+        private WebhookTypesRegistry $webhookTypesRegistry,
         private TcaSchemaFactory $tcaSchemaFactory,
         private ActionRegistry $actionRegistry,
         private UriBuilder $uriBuilder,
@@ -126,6 +128,20 @@ final readonly class OpenApiController
                 }
                 $paths[$pathName] = $pathItem;
             }
+        }
+
+        $webhooks = [];
+        foreach ($this->webhookTypesRegistry->getAvailableWebhookTypes() as $identifier => $webhookType) {
+            $webhooks[$identifier] = new PathItem([
+                'post' => [
+                    'description' => $lang->sL($webhookType->getDescription()) ?: $webhookType->getDescription(),
+                    'responses' => [
+                        '200' => [
+                            'description' => 'Return a 200 status to indicate that the data was received successfully',
+                        ],
+                    ],
+                ],
+            ]);
         }
 
         $tags = [];
@@ -219,6 +235,7 @@ final readonly class OpenApiController
             ]),
             'paths' => new Paths($paths),
             'tags' => $tags,
+            'webhooks' => $webhooks,
             'components' => new Components([
                 'schemas' => $schemas,
                 'securitySchemes' => $securitySchemes,
