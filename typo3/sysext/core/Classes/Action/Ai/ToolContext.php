0e9911a48b20c9b0f23bb8ae3cfa57f8176c2e33
Benjamin Franzke <ben@bnf.dev>
2025-12-22 22:04:04 +0100

    [WIP][FEATURE] Provide AI Tool provider based on Actions API
    Change-Id: I14b8fb18295100af456b439df452bef3773baddb

---
+++ typo3/sysext/core/Classes/Action/Ai/ToolContext.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace TYPO3\CMS\Core\Action\Ai;
+
+use Psr\Http\Message\ServerRequestInterface;
+use TYPO3\CMS\Core\Action\ActionContext;
+use TYPO3\CMS\Core\Authentication\AbstractUserAuthentication;
+use TYPO3\CMS\Core\Context\Context;
+use TYPO3\CMS\Core\Localization\LanguageService;
+
+final readonly class ToolContext
+{
+    public function __construct(
+        public AbstractUserAuthentication $principal,
+        public LanguageService $languageService,
+        public ?ServerRequestInterface $request,
+    ) {}
+
+    public static function fromActionContext(ActionContext $context): self
+    {
+        return new self(
+            $context->principal,
+            $context->languageService,
+            $context->request,
+        );
+    }
+}
