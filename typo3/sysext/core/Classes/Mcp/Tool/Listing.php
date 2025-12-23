687c1623932ca982bde551506a802a01ae372f38
Benjamin Franzke <ben@bnf.dev>
2025-12-23 21:27:59 +0100

    [WIP][FEATURE] Add MCP Server demo based on Actions API
    Implement a simple MCP server demo to showcase that the Actions
    API AI-tools adapter can be used to drive an MCP Server.
    
    The MCP server exposes actions from the Actions API as MCP tools
    and is implemented via Actions API itself.
    There's a lot missing, like a filtering for which actions
    shall be exposed in which context. This change is here to prepare
    the Actions API design to be compatible with different downstream
    needs (REST, MCP, OpenAI Tools, and so on…), without requiring
    every functionality to be wrapped for REST and MCP manually.
    
    Releases: main
    Resolves: #
    Change-Id: I2f2ff276cb533422b6aa6393a16d1ea7c9bf55ef

---
+++ typo3/sysext/core/Classes/Mcp/Tool/Listing.php
@@ -0,0 +1,86 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace TYPO3\CMS\Core\Mcp\Tool;
+
+use TYPO3\CMS\Core\Action\Ai\Tool as AiTool;
+use TYPO3\CMS\Core\Action\Ai\ToolProvider;
+use TYPO3\CMS\Core\Mcp\Request;
+use TYPO3\CMS\Core\Mcp\RequestHandlerInterface;
+use TYPO3\CMS\Core\Mcp\Response;
+
+final readonly class Listing implements RequestHandlerInterface
+{
+    public function __construct(
+        private ToolProvider $toolProvider
+    ) {}
+
+    public static function getName(): string
+    {
+        return 'tools/list';
+    }
+
+    public function handle(Request $request): Response
+    {
+        return new Response($request->id, [
+            'tools' => array_map(
+                static fn(AiTool $tool): object => (object)[
+                    'name' => $tool->shortname,
+                    'title' => $tool->summary,
+                    'description' => $tool->description,
+                    'inputSchema' => $tool->inputSchema ?? (object)[
+                        'type' => 'object',
+                        'properties' => (object)[],
+                        'additionalProperties' => false,
+                    ],
+                    ...($tool->outputSchema === null ? [] : (
+                        ($tool->outputSchema->type ?? null) === 'object' ? [
+                            'outputSchema' => $tool->outputSchema,
+                        ] : [
+                            'outputSchema' => [
+                                'type' => 'object',
+                                'properties' => [
+                                    // @todo delete components
+                                    'data' => ($tool->outputSchema->type ?? null) === 'null'
+                                        // @todo see note in Tool/Call regarding "OK" response
+                                        ? ['type' => 'string', 'enum' => ['OK']]
+                                        : $tool->outputSchema,
+                                ],
+                                'additionalProperties' => false,
+                                'required' => ['data'],
+                                '$defs' => $tool->outputSchema->{'$defs'} ?? [],
+                            ],
+                        ]
+                    )),
+                    'annotations' => [
+                        'readOnlyHint' => $tool->isReadOnly,
+                        'destructiveHint' => $tool->isDestructive,
+                        'idempotentHint' => $tool->isIdempotent,
+                    ],
+                ],
+                array_values(
+                    $this->toolProvider->getTools(
+                        array_filter(
+                            array_keys($request->context->scopes),
+                            static fn(string $scope): bool => $scope !== 'mcp',
+                        ),
+                    )
+                ),
+            ),
+        ]);
+    }
+}
