c56847d087ceb79b1ce3064aa38de20d027f3e47
Benjamin Franzke <ben@bnf.dev>
2025-12-23 21:27:59 +0100

    [WIP][FEATURE] Add MCP Server demo based on Actions API
    Implement a simple MCP server demo to showcase that the Actions
    API AI-tools adapter can be used to drive an MCP Server.
    
    The MCP server exposes actions from the Actions API as MCP tools
    and is implemented via Actions API itself.
    There's a lot missing, like a filtering for which actions
    shall be exposed in which context. This change is here to prepare
    the Actions API design to be compatible with different downstream
    needs (REST, MCP, OpenAI Tools, and so on…), without requiring
    every functionality to be wrapped for REST and MCP manually.
    
    Releases: main
    Resolves: #
    Change-Id: I2f2ff276cb533422b6aa6393a16d1ea7c9bf55ef

---
+++ typo3/sysext/core/Classes/Mcp/McpScope.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace TYPO3\CMS\Core\Mcp;
+
+use Symfony\Component\DependencyInjection\Attribute\AsTaggedItem;
+use TYPO3\CMS\Core\Scope\ScopeInterface;
+use TYPO3\CMS\Core\Scope\ScopeUser;
+
+#[AsTaggedItem(index: 'mcp', priority: 1)]
+final readonly class McpScope implements ScopeInterface
+{
+    public function getIdentifier(): string
+    {
+        return 'mcp';
+    }
+
+    public function getName(): string
+    {
+        return 'Model Context Protocol (MCP)';
+    }
+
+    public function getDescription(): string
+    {
+        return 'Allow to query information and send commands via MCP';
+    }
+
+    public function getIcon(): string
+    {
+        return 'actions-chat';
+    }
+
+    public function allowedForUser(ScopeUser $user): bool
+    {
+        return true;
+    }
+}
