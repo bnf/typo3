56a63dc89eea1848970fda502f67801a35994283
Benjamin Franzke <ben@bnf.dev>
2025-12-23 21:27:59 +0100

    [WIP][DEMO][FEATURE] Add MCP Server demo based on Actions API
    Implement a simple MCP server demo to showcase that the Actions
    API AI-tools adapter can be used to drive an MCP Server.
    
    The MCP server exposes actions from the Actions API as MCP tools
    and is implemented via Actions API itself.
    There's a lot missing, like a filtering for which actions
    shall be exposed in which context. This change is here to prepare
    the Actions API design to be compatible with different downstream
    needs (REST, MCP, OpenAI Tools, and so on…), without requiring
    every functionality to be wrapped for REST and MCP manually.
    
    Releases: main
    Resolves: #
    Change-Id: I2f2ff276cb533422b6aa6393a16d1ea7c9bf55ef

---
+++ typo3/sysext/core/Classes/Mcp/Tool/Call.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace TYPO3\CMS\Core\Mcp\Tool;
+
+use TYPO3\CMS\Core\Action\Ai\ToolContext;
+use TYPO3\CMS\Core\Action\Ai\ToolProvider;
+use TYPO3\CMS\Core\Mcp\Request;
+use TYPO3\CMS\Core\Mcp\RequestHandlerInterface;
+use TYPO3\CMS\Core\Mcp\Response;
+
+final readonly class Call implements RequestHandlerInterface
+{
+    public function __construct(
+        private ToolProvider $toolProvider
+    ) {}
+
+    public static function getName(): string
+    {
+        return 'tools/call';
+    }
+
+    public function handle(Request $request): Response
+    {
+        $toolName = (string)$request->params['name'];
+        $tool = $this->toolProvider->getTool($toolName);
+        $arguments = (array)$request->params['arguments'];
+
+        try {
+            // @todo validate arguments against json-schema
+            $result = ($tool->handler)($arguments, ToolContext::fromActionContext($request->context));
+            // @todo validate result against json-schema
+            // @todo catch invalid arguments and expose as
+            //       -32602 Invalid params (Invalid method parameter(s)).
+        } catch (\RuntimeException $e) {
+            return new Response($request->id, [
+                'content' => [
+                    (object)[
+                        'type' => 'text',
+                        'text' => $e->getMessage(),
+                    ],
+                ],
+                'isError' => true,
+            ]);
+        }
+
+        $type = $tool->outputSchema->type ?? null;
+        if ($type !== 'object') {
+            $result = ['data' => $result];
+        }
+
+        return new Response($request->id, [
+            // We only support MCP v2025-06-18, therefore no BC compatible `content`
+            // result is returned here. That would look like:
+            //   'content' => [(object)['type' => 'text', 'text' => json_encode($result)],
+            'structuredContent' => $result,
+        ]);
+    }
+}
