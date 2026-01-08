38a3dd4459dc93584953169e451b8f2db1d66269
Benjamin Franzke <ben@bnf.dev>
2026-01-08 16:57:43 +0100

    [TASK] Migrate dashboard to Actions API
    Releases: main
    Resolves: #
    Change-Id: I0fcc4e68d0c10918fe6fd761137f5023d57f4ea6

---
+++ typo3/sysext/dashboard/Classes/Scope/DashboardWriteScope.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace TYPO3\CMS\Dashboard\Scope;
+
+use Symfony\Component\DependencyInjection\Attribute\AsTaggedItem;
+use TYPO3\CMS\Backend\Module\ModuleProvider;
+use TYPO3\CMS\Core\Authentication\BackendUserAuthentication;
+use TYPO3\CMS\Core\Scope\ScopeInterface;
+use TYPO3\CMS\Core\Scope\ScopeUser;
+
+#[AsTaggedItem(index: 'dashboard:write', priority: 49)]
+final readonly class DashboardWriteScope implements ScopeInterface
+{
+    public function __construct(
+        private ModuleProvider $moduleProvider,
+    ) {}
+
+    public function getIdentifier(): string
+    {
+        return 'dashboard:write';
+    }
+
+    public function getName(): string
+    {
+        return 'Dashboard write access';
+    }
+
+    public function getDescription(): string
+    {
+        return 'Create dashboads or widgets';
+    }
+
+    public function getIcon(): string
+    {
+        return 'module-dashboard';
+    }
+
+    public function allowedForUser(ScopeUser $user): bool
+    {
+        return $user->user instanceof BackendUserAuthentication
+            && $this->moduleProvider->accessGranted('dashboard', $user->user);
+    }
+}
