9da316579d192a4ef4104e0f8b8e113480771e82
Benjamin Franzke <ben@bnf.dev>
2026-01-22 14:40:59 +0100

    frontend-app
    Change-Id: I5dec7d3ef7a980b06c89ac2eb1dc9b342bb4f277

--- typo3/sysext/hub/Configuration/TCA/sys_app.php
+++ typo3/sysext/hub/Configuration/TCA/sys_app.php
@@ -77,6 +77,7 @@ return [
                     // @todo generate via `AppType::cases()` and add labels to enum
                     ['label' => 'Static token', 'value' => AppType::STATIC->value],
                     ['label' => 'OAuth', 'value' => AppType::OAUTH->value],
+                    ['label' => 'Frontend', 'value' => 'frontend'],
                 ],
                 'dbFieldLength' => 255,
             ],
