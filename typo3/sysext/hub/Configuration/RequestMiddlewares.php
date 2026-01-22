a4c886b22e634bb9f7693f0bb1f9ec677cad8e69
Benjamin Franzke <ben@bnf.dev>
2026-01-22 14:40:59 +0100

    frontend-app
    Change-Id: I5dec7d3ef7a980b06c89ac2eb1dc9b342bb4f277

--- typo3/sysext/hub/Configuration/RequestMiddlewares.php
+++ typo3/sysext/hub/Configuration/RequestMiddlewares.php
@@ -5,6 +5,7 @@
  */
 
 use TYPO3\CMS\Hub\Http\Middleware\AppResolver;
+use TYPO3\CMS\Hub\Http\Middleware\FrontendAppResolver;
 use TYPO3\CMS\Hub\Http\Middleware\Metadata;
 
 return [
@@ -21,6 +22,20 @@ return [
             'target' => AppResolver::class,
             'before' => [
                 'typo3/cms-backend/authentication',
+            ],
+        ],
+    ],
+    'frontend' => [
+        'typo3/cms-hub/frontend-resolver' => [
+            'target' => FrontendAppResolver::class,
+            'after' => [
+                'typo3/cms-frontend/site',
+            ],
+            'before' => [
+                'typo3/cms-core/request-token-middleware',
+                'typo3/cms-frontend/backend-user-authentication',
+                'typo3/cms-frontend/authentication',
+                'typo3/cms-frontend/page-resolver',
             ],
         ],
     ],
