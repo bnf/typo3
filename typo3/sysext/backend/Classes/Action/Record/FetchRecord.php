cee96a80de14bd5a09ce3045ce6a3a0aec27637d
Benjamin Franzke <ben@bnf.dev>
2026-01-30 09:37:56 +0100

    WIP-serialize
    Change-Id: I6ecf33f5188b6caa87ee8213494a684ea8ce2a75

--- typo3/sysext/backend/Classes/Action/Record/FetchRecord.php
+++ typo3/sysext/backend/Classes/Action/Record/FetchRecord.php
@@ -20,8 +20,8 @@ namespace TYPO3\CMS\Backend\Action\Record;
 use TYPO3\CMS\Backend\Utility\BackendUtility;
 use TYPO3\CMS\Core\Action\ActionContext;
 use TYPO3\CMS\Core\Attribute\AsAction;
+use TYPO3\CMS\Core\Domain\Dto\Record as RecordDto;
 use TYPO3\CMS\Core\Domain\RecordFactory;
-use TYPO3\CMS\Core\Domain\RecordInterface;
 use TYPO3\CMS\Core\Domain\RecordSerializer;
 
 final readonly class FetchRecord
@@ -34,8 +34,7 @@ final readonly class FetchRecord
     /**
      * @template T of string
      * @param T $schema
-     * @return array{record: mixed}
-     * @todo use return array{record: RecordInterface<T>}
+     * @return array{record: RecordDto<T>}
      */
     #[AsAction(
         name: 'records/{schema}/{identifier}',
@@ -52,10 +51,13 @@ final readonly class FetchRecord
             throw new \RuntimeException('Record not found', 1766253177);
         }
 
-        $record = $this->recordFactory->createResolvedRecordFromDatabaseRow($schema, $recordRow);
+        $record = new RecordDto(
+            $this->recordFactory->createResolvedRecordFromDatabaseRow($schema, $recordRow),
+            $this->recordSerializer,
+        );
         return [
             // @todo let record serializer run automatically on action responses
-            'record' => $this->recordSerializer->serialize($record),
+            'record' => $record,
         ];
     }
 }
