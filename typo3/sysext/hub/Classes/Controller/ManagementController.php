9f23b4d6c48cd4030f5c894a07435365b0d213c3
Benjamin Franzke <ben@bnf.dev>
2026-04-09 10:32:13 +0200

    [WIP][FEATURE] Implement API Hub
    Provides API access to the TYPO3 Actions API
    via OAuth and static App tokens.
    
    composer require league/oauth2-server
    composer require -d typo3/sysext/core --no-update \
      league/oauth2-server
    
    Releases: main
    Resolves: #
    Change-Id: I4c2bf613b592221f458cf88c74c8fe4f6ee4bd9d

---
+++ typo3/sysext/hub/Classes/Controller/ManagementController.php
@@ -0,0 +1,153 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace TYPO3\CMS\Hub\Controller;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use TYPO3\CMS\Backend\Attribute\AsController;
+use TYPO3\CMS\Backend\Routing\UriBuilder;
+use TYPO3\CMS\Backend\Template\Components\ComponentFactory;
+use TYPO3\CMS\Backend\Template\Components\MultiRecordSelection\Action;
+use TYPO3\CMS\Backend\Template\ModuleTemplate;
+use TYPO3\CMS\Backend\Template\ModuleTemplateFactory;
+use TYPO3\CMS\Core\Imaging\IconFactory;
+use TYPO3\CMS\Core\Imaging\IconSize;
+use TYPO3\CMS\Core\Localization\LanguageService;
+use TYPO3\CMS\Core\Pagination\SimplePagination;
+use TYPO3\CMS\Hub\Pagination\DemandedArrayPaginator;
+use TYPO3\CMS\Hub\Repository\AppDemand;
+use TYPO3\CMS\Hub\Repository\AppRepository;
+
+/**
+ * The System > App module: Rendering the listing of hub.
+ *
+ * @internal This class is a specific Backend controller implementation and is not part of the TYPO3's Core API.
+ */
+#[AsController]
+class ManagementController
+{
+    public function __construct(
+        private readonly UriBuilder $uriBuilder,
+        private readonly IconFactory $iconFactory,
+        private readonly ModuleTemplateFactory $moduleTemplateFactory,
+        private readonly AppRepository $appRepository,
+        private readonly ComponentFactory $componentFactory,
+    ) {}
+
+    public function overviewAction(ServerRequestInterface $request): ResponseInterface
+    {
+        $view = $this->moduleTemplateFactory->create($request);
+        $demand = AppDemand::fromRequest($request);
+
+        $this->registerDocHeaderButtons($view, $request->getAttribute('normalizedParams')->getRequestUri(), $demand);
+        $view->makeDocHeaderModuleMenu();
+
+        $appRecords = $this->appRepository->getAppRecords($demand);
+        $paginator = new DemandedArrayPaginator($appRecords, $demand->getPage(), $demand->getLimit(), $this->appRepository->countAll($demand));
+        $pagination = new SimplePagination($paginator);
+
+        $requestUri = $request->getAttribute('normalizedParams')->getRequestUri();
+        $languageService = $this->getLanguageService();
+
+        return $view->assignMultiple([
+            'demand' => $demand,
+            'paginator' => $paginator,
+            'pagination' => $pagination,
+            'actions' => [
+                new Action(
+                    'edit',
+                    [
+                        'idField' => 'uid',
+                        'tableName' => 'sys_app',
+                        'returnUrl' => $requestUri,
+                    ],
+                    'actions-open',
+                    'core.core:cm.edit'
+                ),
+                new Action(
+                    'delete',
+                    [
+                        'idField' => 'uid',
+                        'tableName' => 'sys_app',
+                        'title' => $languageService->translate('labels.delete.title', 'hub.module'),
+                        'content' => $languageService->translate('labels.delete.message', 'hub.module'),
+                        'ok' => $languageService->translate('cm.delete', 'core.core'),
+                        'cancel' => $languageService->translate('labels.cancel', 'core.core'),
+                        'returnUrl' => $requestUri,
+                    ],
+                    'actions-edit-delete',
+                    'core.core:cm.delete'
+                ),
+            ],
+        ])->renderResponse('Management/Overview');
+    }
+
+    public function swaggerAction(ServerRequestInterface $request): ResponseInterface
+    {
+        $view = $this->moduleTemplateFactory->create($request);
+        $demand = AppDemand::fromRequest($request);
+
+        $this->registerDocHeaderButtons($view, $request->getAttribute('normalizedParams')->getRequestUri(), $demand);
+        $view->makeDocHeaderModuleMenu();
+
+        return $view->renderResponse('Management/Swagger');
+    }
+
+    protected function registerDocHeaderButtons(ModuleTemplate $view, string $requestUri, AppDemand $demand): void
+    {
+        $languageService = $this->getLanguageService();
+
+        $newRecordButton = $this->componentFactory->createLinkButton()
+            ->setHref((string)$this->uriBuilder->buildUriFromRoute(
+                'record_edit',
+                [
+                    'edit' => ['sys_app' => ['new']],
+                    'module' => 'integrations_hub',
+                    'returnUrl' => (string)$this->uriBuilder->buildUriFromRoute('integrations_hub'),
+                ]
+            ))
+            ->setShowLabelText(true)
+            ->setTitle($languageService->translate('app_create', 'hub.module'))
+            ->setIcon($this->iconFactory->getIcon('actions-plus', IconSize::SMALL));
+        $view->getDocHeaderComponent()->getButtonBar()->addButton($newRecordButton);
+
+        $swaggerButton = $this->componentFactory->createLinkButton()
+            ->setHref((string)$this->uriBuilder->buildUriFromRoute(
+                'integrations_hub.swagger',
+            ))
+            ->setShowLabelText(true)
+            ->setTitle('API documentation')
+            ->setIcon($this->iconFactory->getIcon('actions-document', IconSize::SMALL));
+        $view->getDocHeaderComponent()->getButtonBar()->addButton($swaggerButton);
+
+        $view->getDocHeaderComponent()->setShortcutContext(
+            'integrations_hub',
+            $languageService->translate('title', 'hub.module'),
+            array_filter([
+                'demand' => $demand->getParameters(),
+                'orderField' => $demand->getOrderField(),
+                'orderDirection' => $demand->getOrderDirection(),
+            ])
+        );
+    }
+
+    protected function getLanguageService(): LanguageService
+    {
+        return $GLOBALS['LANG'];
+    }
+}
---
+++ typo3/sysext/hub/Configuration/TCA/sys_app.php
@@ -0,0 +1,161 @@
+<?php
+
+use TYPO3\CMS\Hub\Form\ItemsProcFunc\AppScopes;
+use TYPO3\CMS\Hub\Type\AppType;
+
+return [
+    'ctrl' => [
+        'title' => 'hub.db:sys_app',
+        'label' => 'name',
+        'descriptionColumn' => 'description',
+        'crdate' => 'createdon',
+        'tstamp' => 'updatedon',
+        'adminOnly' => true,
+        'hideTable' => true,
+        'rootLevel' => 1,
+        'groupName' => 'system',
+        'default_sortby' => 'name',
+        'type' => 'app_type',
+        'typeicon_column' => 'app_type',
+        'typeicon_classes' => [
+            'default' => 'content-webhook', // @todo Change to "content-app" when available
+        ],
+        'delete' => 'deleted',
+        'enablecolumns' => [
+            'disabled' => 'disabled',
+            'starttime' => 'starttime',
+            'endtime' => 'endtime',
+        ],
+        'versioningWS_alwaysAllowLiveEdit' => true,
+    ],
+    'types' => [
+        'static' => [
+            'showitem' => '
+                --div--;core.form.tabs:general,
+                --palette--;;config,
+                impersonate_user,
+                --div--;core.form.tabs:access,
+                --palette--;;access',
+            'columnsOverrides' => [
+                'impersonate_user' => [
+                    'config' => [
+                        'required' => true,
+                    ],
+                ],
+            ],
+        ],
+        'oauth' => [
+            'showitem' => '
+                --div--;core.form.tabs:general,
+                --palette--;;config,
+                redirect_uri,
+                --div--;core.form.tabs:access,
+                --palette--;;access',
+        ],
+    ],
+    'palettes' => [
+        'config' => [
+            'label' => 'hub.db:palette.config',
+            'description' => 'hub.db:palette.config.description',
+            'showitem' => 'app_type, --linebreak--, name, description, --linebreak--, identifier, secret, --linebreak--, scopes, --linebreak--, logo',
+        ],
+        'access' => [
+            'label' => 'core.form.palettes:access',
+            'showitem' => 'disabled, starttime, endtime',
+        ],
+    ],
+    'columns' => [
+        'app_type' => [
+            'label' => 'hub.db:sys_app.app_type',
+            'description' => 'hub.db:sys_app.app_type.description',
+            'config' => [
+                'type' => 'select',
+                'renderType' => 'selectSingle',
+                'required' => true,
+                'items' => [
+                    //['label' => 'hub.db:sys_app.app_type.select', 'value' => ''],
+                    // @todo generate via `AppType::cases()` and add labels to enum
+                    ['label' => 'Static token', 'value' => AppType::STATIC->value],
+                    ['label' => 'OAuth', 'value' => AppType::OAUTH->value],
+                ],
+                'dbFieldLength' => 255,
+            ],
+        ],
+        'name' => [
+            'label' => 'hub.db:sys_app.name',
+            'description' => 'hub.db:sys_app.name.description',
+            'config' => [
+                'type' => 'input',
+                'required' => true,
+                'max' => 100,
+                'eval' => 'trim',
+            ],
+        ],
+        'identifier' => [
+            'label' => 'hub.db:sys_app.identifier',
+            'description' => 'hub.db:sys_app.identifier.description',
+            'config' => [
+                'type' => 'uuid',
+            ],
+        ],
+        'secret' => [
+            'label' => 'hub.db:sys_app.secret',
+            'description' => 'hub.db:sys_app.secret.description',
+            'config' => [
+                'type' => 'password',
+                'required' => true,
+                'fieldControl' => [
+                    'tokenGenerator' => [
+                        'renderType' => 'tokenGenerator',
+                        'options' => [
+                            'title' => 'hub.db:sys_app.secret.passwordGenerator',
+                            'allowEdit' => false,
+                            'passwordRules' => [
+                                'length' => 40,
+                                'random' => 'hex',
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ],
+        'scopes' => [
+            'label' => 'hub.db:sys_app.scopes',
+            'description' => 'hub.db:sys_app.scopes.description',
+            'config' => [
+                'type' => 'select',
+                'renderType' => 'selectCheckBox',
+                'itemsProcFunc' => AppScopes::class . '->provideTcaSelectItems',
+            ],
+        ],
+        'logo' => [
+            'label' => 'Logo',
+            'config' => [
+                'type' => 'file',
+                'allowed' => 'common-image-types',
+                'appearance' => [
+                    'createNewRelationLinkTitle' => 'frontend.ttc:images.addFileReference',
+                ],
+            ],
+        ],
+        'redirect_uri' => [
+            'label' => 'hub.db:sys_app.redirect_uri',
+            'description' => 'hub.db:sys_app.redirect_uri.description',
+            'config' => [
+                'type' => 'link',
+                'allowedTypes' => ['url'],
+            ],
+        ],
+        'impersonate_user' => [
+            'label' => 'hub.db:sys_app.impersonate_user',
+            'description' => 'hub.db:sys_app.impersonate_user.description',
+            'config' => [
+                'type' => 'group',
+                'allowed' => 'be_users',
+                'size' => 1,
+                'relationship' => 'manyToOne',
+                'nullable' => true,
+            ],
+        ],
+    ],
+];
